103271caad6258532fee9f0457dbe7de7d2c7142
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-05-12 17:56:36 +0300

    Horizontal speed

--- ANDOR-CS/Enums/OutputAmplificationEnum.cs
+++ ANDOR-CS/Enums/OutputAmplificationEnum.cs
@@ -6,9 +6,24 @@ using System.Threading.Tasks;
 
 namespace ANDOR_CS.Enums
 {
+    /// <summary>
+    /// Different type of output amplifiers. Availalbe on iXon, Clara and Newton
+    /// </summary>
     public enum OutputAmplification : uint
     {
+        /// <summary>
+        /// Electron multiplication. Not available for Clara 
+        /// </summary>
         ElectromMultiplication = 0,
-        Conventional = 1
+
+        /// <summary>
+        /// Standard conventional 
+        /// </summary>
+        Conventional = 1,
+
+        /// <summary>
+        /// Only supported by Clara
+        /// </summary>
+        ExtendedNIR = 2
     }
 }
