ec24ce37d29c51395ac9d334636ab0972e987379
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-05-15 17:52:42 +0300

    AcquisitionSettings
    General improvememnts

--- ANDOR-CS/Classes/AcquisitionSettings.cs
+++ ANDOR-CS/Classes/AcquisitionSettings.cs
@@ -19,6 +19,28 @@ namespace ANDOR_CS.Classes
     /// </summary>
     public class AcquisitionSettings
     {
+
+        private static readonly Tuple<AcquisitionMode, int>[] AcquisitionModeTable = 
+         {
+            new Tuple<AcquisitionMode, int>(Enums.AcquisitionMode.SingleScan, 1),
+            new Tuple<AcquisitionMode, int>(Enums.AcquisitionMode.Accumulation, 2),
+            new Tuple<AcquisitionMode, int>(Enums.AcquisitionMode.Kinetic, 3),
+            new Tuple<AcquisitionMode, int>(Enums.AcquisitionMode.FastKinetics, 4),
+            new Tuple<AcquisitionMode, int>(Enums.AcquisitionMode.RunTillAbort, 5)
+        };
+
+        private static readonly Tuple<ReadMode, int>[] ReadModeTable =
+        {
+            new Tuple<ReadMode, int>(Enums.ReadMode.FullVerticalBinning, 0),
+            new Tuple<ReadMode, int>(Enums.ReadMode.MultiTrack, 1),
+            new Tuple<ReadMode, int>(Enums.ReadMode.RandomTrack, 2),
+            new Tuple<ReadMode, int>(Enums.ReadMode.SingleTrack,3),
+            new Tuple<ReadMode, int>(Enums.ReadMode.FullImage, 4),
+            new Tuple<ReadMode, int>(Enums.ReadMode.SubImage, 5)
+        };
+
+
+ 
         /// <summary>
         /// A reference to the parent <see cref="Camera"/> object.
         /// Used to perform checks of capabilities of the current camera.
@@ -71,7 +93,41 @@ namespace ANDOR_CS.Classes
             private set;
         } = null;
 
+        /// <summary>
+        /// Stores type of currently set PreAmp Gain
+        /// </summary>
+        public Tuple<int, string> PreAmpGain
+        {
+            get;
+            private set;
+        }
 
+        /// <summary>
+        /// Stores currently set acquisition mode
+        /// </summary>
+        public Tuple<int, AcquisitionMode> AcquisitionMode
+        {
+            get;
+            private set;
+        } = null;
+
+        /// <summary>
+        /// Stores currently set read mode
+        /// </summary>
+        public Tuple<int, ReadMode> ReadMode
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Stores exposure time
+        /// </summary>
+        public float? ExposureTime
+        {
+            get;
+            private set;
+        } = null;
 
 
         /// <summary>
@@ -105,7 +161,91 @@ namespace ANDOR_CS.Classes
                 throw new AndorSDKException("Camera is not currently active.", null);
         }
 
-        
+        /// <summary>
+        /// Applys currenlty selected settings to the camera.
+        /// </summary>
+        /// <exception cref="ArgumentNullException"/>
+        /// <returns>Result of application of each non-null setting</returns>
+        public List<Tuple<string, bool, uint>> ApplySettings()
+        {
+            List<Tuple<string, bool, uint>> output = new List<Tuple<string, bool, uint>>();
+
+            CheckCamera(true);
+
+            uint result = 0;
+
+
+            if (VSSpeed != null)
+            {
+                result = SDKInit.SDKInstance.SetVSSpeed(VSSpeed.Item1);
+
+                output.Add(new Tuple<string, bool, uint>("VS Speed", result == SDK.DRV_SUCCESS, result));
+            }
+
+            if (VSAmplitude != null)
+            {
+                result = SDKInit.SDKInstance.SetVSAmplitude((int)VSAmplitude);
+
+                output.Add(new Tuple<string, bool, uint>("VS Amplitude", result == SDK.DRV_SUCCESS, result));
+            }
+
+            if (ADConverter != null)
+            {
+                result = SDKInit.SDKInstance.SetADChannel(ADConverter.Item1);
+
+                output.Add(new Tuple<string, bool, uint>("AD Converter", result == SDK.DRV_SUCCESS, result));
+            }
+
+            if (Amplifier != null)
+            {
+                result = SDKInit.SDKInstance.SetOutputAmplifier(Amplifier.Item3);
+
+                output.Add(new Tuple<string, bool, uint>("Amplifier", result == SDK.DRV_SUCCESS, result));
+            }
+
+            if (HSSpeed != null)
+            {
+                result = SDKInit.SDKInstance.SetHSSpeed(Amplifier?.Item3 ?? 0, HSSpeed.Item1);
+
+                output.Add(new Tuple<string, bool, uint>("HS Speed", result == SDK.DRV_SUCCESS, result));
+            }
+
+            if (PreAmpGain != null)
+            {
+                result = SDKInit.SDKInstance.SetPreAmpGain(PreAmpGain.Item1);
+
+                output.Add(new Tuple<string, bool, uint>("PreAmp Gain", result == SDK.DRV_SUCCESS, result));
+            }
+
+            if (AcquisitionMode != null)
+            {
+                result = SDKInit.SDKInstance.SetAcquisitionMode(AcquisitionMode.Item1);
+
+                output.Add(new Tuple<string, bool, uint>("Acquisition mode", result == SDK.DRV_SUCCESS, result));
+            }
+            else throw new ArgumentNullException("Acquisition mode should be set before applying settings.");
+
+
+            if (ReadMode != null)
+            {
+                result = SDKInit.SDKInstance.SetReadMode(ReadMode.Item1);
+
+                output.Add(new Tuple<string, bool, uint>("Read mode", result == SDK.DRV_SUCCESS, result));
+
+            }
+            else throw new ArgumentNullException("Read mode should be set before applying settings.");
+
+            if (ExposureTime.HasValue)
+            {
+                result = SDKInit.SDKInstance.SetExposureTime(ExposureTime.Value);
+
+                output.Add(new Tuple<string, bool, uint>("Exposure time", result == SDK.DRV_SUCCESS, result));
+            }
+            else throw new ArgumentNullException("Exposure time should be set before applying settings.");
+
+            return output;
+        }
+
 
         /// <summary>
         /// Tries to set vertical speed. 
@@ -130,6 +270,7 @@ namespace ANDOR_CS.Classes
                 if (speedIndex < 0 || speedIndex >= length)
                     throw new ArgumentOutOfRangeException($"{nameof(speedIndex)} is out of range (should be in [{0},  {length-1}]).");
 
+                
                 // If success, updates VSSpeed field and 
                 VSSpeed = new Tuple<int, float>(speedIndex, camera.Properties.VSSpeeds[speedIndex]);
                
@@ -256,7 +397,6 @@ namespace ANDOR_CS.Classes
         /// <exception cref="NullReferenceException"/>
         /// <exception cref="ArgumentOutOfRangeException"/>
         /// <exception cref="NotSupportedException"/>
-        /// <exception cref="AndorSDKException"/>
         /// <returns>An enumerable collection of speed indexes and respective speed values available.</returns>
         public IEnumerable<Tuple<int, float>> GetAvailableHSSpeeds()
         {
@@ -297,7 +437,7 @@ namespace ANDOR_CS.Classes
                
             }
             else
-                throw new NotSupportedException("Camera does not support horizontal readout speed controls");
+                throw new NotSupportedException("Camera does not support horizontal readout speed controls.");
 
 
         }
@@ -305,14 +445,13 @@ namespace ANDOR_CS.Classes
         /// <summary>
         /// Sets Horizontal Readout Speed for currently selected Amplifier and AD Converter.
         /// Requires camera to be active.
-        /// Note: <see cref="AcquisitionSettings.ADConverter"/> amd <see cref="AcquisitionSettings.Amplifier"/> should be set
+        /// Note: <see cref="AcquisitionSettings.ADConverter"/> and <see cref="AcquisitionSettings.Amplifier"/> should be set
         /// via <see cref="AcquisitionSettings.SetADConverter(int)"/> and <see cref="AcquisitionSettings.SetOutputAmplifier(OutputAmplification)"/>
         /// before calling this method.
         /// </summary>
         /// <exception cref="NullReferenceException"/>
         /// <exception cref="ArgumentOutOfRangeException"/>
         /// <exception cref="NotSupportedException"/>
-        /// <exception cref="AndorSDKException"/>
         /// <param name="speedIndex">Index of horizontal speed</param>
         public void SetHSSpeed(int speedIndex)
         {
@@ -351,6 +490,165 @@ namespace ANDOR_CS.Classes
             }
             else
                 throw new NotSupportedException("Camera does not support horizontal readout speed controls");       
+        }
+
+        /// <summary>
+        /// Returns a collection of available PreAmp gains for currently selected HSSpeed, Amplifier, Converter.
+        /// Requires camera to be active.
+        /// Note: <see cref="AcquisitionSettings.ADConverter"/>, <see cref="AcquisitionSettings.HSSpeed"/>
+        /// and <see cref="AcquisitionSettings.Amplifier"/> should be set
+        /// via <see cref="AcquisitionSettings.SetADConverter(int)"/>, <see cref="AcquisitionSettings.SetHSSpeed(int)"/>
+        /// and <see cref="AcquisitionSettings.SetOutputAmplifier(OutputAmplification)"/>.
+        /// </summary>
+        /// <exception cref="NullReferenceException"/>
+        /// <exception cref="NotSupportedException"/>
+        /// <returns>Available PreAmp gains</returns>
+        public IEnumerable<Tuple<int, string>> GetAvailablePreAmpGain()
+        {
+            // Checks if camera is OK and is active
+            CheckCamera(true);
+
+            // Checks if camera supports PreAmp Gain control
+            if (camera.Capabilities.SetFunctions.HasFlag(SetFunction.PreAmpGain))
+            {
+                // Check if all required settings are already set
+                if (HSSpeed == null || Amplifier == null || ADConverter == null)
+                    throw new NullReferenceException($"One of the following settings are not set: AD Converter ({nameof(ADConverter)})," +
+                        $"Amplifier ({nameof(Amplifier)}), Vertical Speed ({nameof(VSSpeed)}).");
+
+
+                // Stores indexes of ADConverter, Amplifier and Horizontal Speed
+                int channel = ADConverter.Item1;
+                int amp = Amplifier.Item3;
+                int speed = HSSpeed.Item1;
+
+                // Total number of gain settings available
+                int gainNumber = camera.Properties.PreAmpGains.Length;
+
+
+                for (int gainIndex = 0; gainIndex < gainNumber; gainIndex++)
+                {
+                    int status = -1;
+
+                    uint result = SDKInit.SDKInstance.IsPreAmpGainAvailable(channel, amp, speed, gainIndex, ref status);
+                    ThrowIfError(result, nameof(SDKInit.SDKInstance.IsPreAmpGainAvailable));
+
+                    // If status of a certain combination of settings is 1, return it
+                    if (status == 1)
+                        yield return new Tuple<int, string>(gainIndex, camera.Properties.PreAmpGains[gainIndex]);
+                }
+
+            }
+            else
+                throw new NotSupportedException("Camera does not support Pre Amp Gain controls.");
+        }
+
+        /// <summary>
+        /// Sets PreAmp gain for currently selected HSSpeed, Amplifier, Converter.
+        /// Requires camera to be active.
+        /// Note: <see cref="AcquisitionSettings.ADConverter"/>, <see cref="AcquisitionSettings.HSSpeed"/>
+        /// and <see cref="AcquisitionSettings.Amplifier"/> should be set
+        /// via <see cref="AcquisitionSettings.SetADConverter(int)"/>, <see cref="AcquisitionSettings.SetHSSpeed(int)"/>
+        /// and <see cref="AcquisitionSettings.SetOutputAmplifier(OutputAmplification)"/>.
+        /// </summary>
+        /// <exception cref="NullReferenceException"/>
+        /// <exception cref="ArgumentOutOfRangeException"/>
+        /// <param name="gainIndex">Index of gain</param>
+        public void SetPreAmpGain(int gainIndex)
+        {
+            // Checks if camera is OK and is active
+            CheckCamera(true);
+
+            // Checks if camera supports PreAmp Gain control
+            if (camera.Capabilities.SetFunctions.HasFlag(SetFunction.PreAmpGain))
+            {
+                // Check if all required settings are already set
+                if (HSSpeed == null || Amplifier == null || ADConverter == null)
+                    throw new NullReferenceException($"One of the following settings are not set: AD Converter ({nameof(ADConverter)})," +
+                        $"Amplifier ({nameof(Amplifier)}), Vertical Speed ({nameof(VSSpeed)}).");
+
+
+                // Stores indexes of ADConverter, Amplifier and Horizontal Speed
+                int channel = ADConverter.Item1;
+                int amp = Amplifier.Item3;
+                int speed = HSSpeed.Item1;
+
+                // Total number of gain settings available
+                int gainNumber = camera.Properties.PreAmpGains.Length;
+
+                // Checks if argument is in valid range
+                if (gainIndex < 0 || gainIndex >= gainNumber)
+                    throw new ArgumentOutOfRangeException($"Gain index (nameof{gainIndex}) is out of range (should be in [{0}, {gainNumber}]).");
+
+                int status = -1;
+
+                uint result = SDKInit.SDKInstance.IsPreAmpGainAvailable(channel, amp, speed, gainIndex, ref status);
+                ThrowIfError(result, nameof(SDKInit.SDKInstance.IsPreAmpGainAvailable));
+
+                if (status != 1)
+                    throw new ArgumentOutOfRangeException($"Pre amp gain index ({gainIndex}) is out of range.");
+
+                PreAmpGain = new Tuple<int, string>(gainIndex, camera.Properties.PreAmpGains[gainIndex]);
+            }
+        }
+
+        /// <summary>
+        /// Sets acquisition mode. 
+        /// Camera may be inactive.
+        /// </summary>
+        /// <exception cref="NotSupportedException"/>
+        /// <param name="mode">Acquisition mode</param>
+        public void SetAcquisitionMode(AcquisitionMode mode)
+        {
+            // Checks if camera is OK
+            CheckCamera(false);
+
+            // Checks if camera supports specifed mode
+            if (!camera.Capabilities.AcquisitionModes.HasFlag(mode))
+                throw new NotSupportedException($"Camera does not support specified regime ({Extensions.GetEnumNames(typeof(AcquisitionMode), mode).First()})");
+
+            // Deternines SDK index of mode
+            int index = AcquisitionModeTable.Where((x) => x.Item1 == mode).FirstOrDefault().Item2;
+
+            AcquisitionMode = new Tuple<int, Enums.AcquisitionMode>(index, mode);
+        }
+
+
+        /// <summary>
+        /// Sets read mode.
+        /// Camera may be inactive.
+        /// </summary>
+        /// <exception cref="NotSupportedException"/>
+        /// <param name="mode">Read mode</param>
+        public void SetReadoutMode(ReadMode mode)
+        {
+            // Checks camera
+            CheckCamera(false);
+
+            // Checks if camera support read mode control
+            if(!camera.Capabilities.ReadModes.HasFlag(mode))
+                throw new NotSupportedException($"Camera does not support specified regime ({Extensions.GetEnumNames(typeof(ReadMode), mode).First()})");
+
+            // Determines SDK index of mode
+            int index = ReadModeTable.Where((x) => x.Item1 == mode).FirstOrDefault().Item2;
+
+            ReadMode = new Tuple<int, Enums.ReadMode>(index, mode);
+        }
+
+        /// <summary>
+        /// Sets exposure time.
+        /// Camera may be inactive.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException"/>
+        /// <param name="time">Exposure time</param>
+        public void SetExposureTime(float time)
+        {
+            // If time is negative throws exception
+            if (time < 0)
+                throw new ArgumentOutOfRangeException($"Exposure time cannot be less than 0 (provided {time}).");
+
+
+            ExposureTime = time;
         }
     }
 }
