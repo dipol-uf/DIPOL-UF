52a4d46b45f5c51e38ea5179aed1f4292c10587f
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-05-24 18:08:27 +0300

    Temperature cycle support; Asynch improvements

--- ANDOR-CS/Classes/AndorSDKInitialization.cs
+++ ANDOR-CS/Classes/AndorSDKInitialization.cs
@@ -12,13 +12,56 @@ namespace ANDOR_CS
     /// </summary>
     public static class AndorSDKInitialization
     {
+
+        private static Random R = new Random();
+        private static ATMCD64CS.AndorSDK _SDKInstance = new ATMCD64CS.AndorSDK();
+
+        private static System.Threading.SemaphoreSlim semaphore = new System.Threading.SemaphoreSlim(1, 1);
+
         /// <summary>
         /// Gets an singleton instance of a basic AndorSDK class
         /// </summary>
         public static ATMCD64CS.AndorSDK SDKInstance
         {
-            get;
-            private set;
-        } = new ATMCD64CS.AndorSDK();
+            get
+            {
+                byte[] arr = new byte[4];
+
+                R.NextBytes(arr);
+
+                int handle = BitConverter.ToInt32(arr, 0);
+
+                bool entered = false;
+
+                try
+                {
+                    semaphore.Wait();
+                    entered = true;
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine($"Semaphore entered. ({handle.ToString("X8")})");
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                    return _SDKInstance;
+                }
+                finally
+                {
+                    if (entered)
+                    {
+                        semaphore.Release();
+                        entered = true;
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        Console.WriteLine($"Semaphore left.    ({handle.ToString("X8")})");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"Semaphore was not entered!. ({handle.ToString("X8")})");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                }
+            }
+            
+        } 
     }
 }
---
+++ ANDOR-CS/Events/TemperatureStatusEventArgs.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ANDOR_CS.Enums;
+
+namespace ANDOR_CS.Events
+{
+
+    /// <summary>
+    /// Stores the event arguments for all Acquisition-based events
+    /// </summary>
+    public class TemperatureStatusEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Time stamp of the event
+        /// </summary>
+        public DateTime EventTime
+        {
+            get;
+            private set;
+        }
+        
+        /// <summary>
+        /// Camera temperature.
+        /// </summary>
+        public float Temperature
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Camera status at the moment of the event
+        /// </summary>
+        public TemperatureStatus Status
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public TemperatureStatusEventArgs(TemperatureStatus status, float temp)
+            : base()
+        {
+            EventTime = DateTime.Now;
+
+            Status = status;
+            Temperature = temp;
+        }
+    }
+}
