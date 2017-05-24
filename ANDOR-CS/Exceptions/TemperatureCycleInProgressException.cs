b40afb154624c48854f8d6124d5970419bd063ec
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-05-24 15:54:48 +0300

    ExtendingAsynchronousSupport

---
+++ ANDOR-CS/Exceptions/TemperatureCycleInProgressException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ANDOR_CS.Classes;
+
+namespace ANDOR_CS.Exceptions
+{
+    public class TemperatureCycleInProgressException : Exception
+    {
+        public TemperatureCycleInProgressException(string message) :
+            base(message)
+        { }
+
+        public static void ThrowIfTempCycle(Camera cam)
+        {
+            if (cam.IsInTemperatureCycle)
+                throw new AcquisitionInProgressException("Camera is in temperature cycle at the moment.");
+        }
+        
+    }
+}
