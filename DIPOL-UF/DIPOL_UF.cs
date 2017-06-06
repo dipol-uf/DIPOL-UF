134b33e04e8c88bf253be1a5c1787c15be973d5d
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-06-06 16:49:43 +0300

    WPF introduced

---
+++ DIPOL-UF/DIPOL_UF.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIPOL_UF
+{
+    public static class DIPOL_UF_App
+    {
+        [STAThread]
+        public static int Main(string[] args)
+        {
+            App applicationInstance = new App();
+            applicationInstance.Run(new DipolMainWindow());
+
+            return 0;
+        }
+    }
+}
