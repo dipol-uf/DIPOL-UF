02c7ac6475985cdeb1c79e5c6065cf1a1a3f0662
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-06-08 11:13:38 +0300

    Device Interface & SHutter control

---
+++ ANDOR-CS/Enums/TTLShutterSignalEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ANDOR_CS.Enums
+{
+    public enum TTLShutterSignal : int
+    {
+        Low = 0,
+        High = 1
+    }
+}
