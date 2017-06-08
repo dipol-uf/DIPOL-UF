c3c2871fe03c671b7b6caba721d68492754c61dd
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-06-08 11:13:38 +0300

    Device Interface & SHutter control

---
+++ ANDOR-CS/Enums/ShutterModeEnum.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ANDOR_CS.Enums
+{
+    public enum ShutterMode : int
+    {
+        FullyAuto = 0,
+        PermanentlyOpen = 1,
+        PermanentlyClosed = 2,
+        OpenForFVBSeries = 4,
+        OpenForAnySeries = 5
+    }
+}
