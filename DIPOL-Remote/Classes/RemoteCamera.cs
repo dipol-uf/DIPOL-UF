a7a77aca9bcbb10c5a5dd112c0eedb23d43f7eb2
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-07-24 19:59:17 +0300

    Some changes to RemoteCamera

---
+++ DIPOL-Remote/Classes/RemoteCamera.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ICameraControl = ANDOR_CS.Interfaces.ICameraControl;
+
+namespace DIPOL_Remote.Classes
+{
+    public class RemoteCamera //: ICameraControl
+    {
+        private RemoteControl session;
+        private int SDKPtr;
+
+        internal RemoteCamera(RemoteControl sessionInstance, int SDKPtr)
+        {
+            session = sessionInstance;
+            this.SDKPtr = SDKPtr;
+        }
+
+
+    }
+}
