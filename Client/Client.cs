24384de292b756a1396bf34537cb95f22d6b2fbb
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-07-24 15:06:51 +0300

    Proper dependencies; Integration with ANDOR classes

--- Client/Client.cs
+++ Client/Client.cs
@@ -17,14 +17,14 @@ namespace Client
 
             Console.WriteLine($"Number of available cameras: {client.Remote.GetNumberOfCameras()}");
 
-            Task.Factory.StartNew(() => 
-            {
-                for (int i = 0; i < 10; i++)
-                {
-                Console.WriteLine(client.Remote.SessionID);
-                    Task.Delay(1000).Wait();
-                }
-            });
+            //Task.Factory.StartNew(() => 
+            //{
+            //    for (int i = 0; i < 10; i++)
+            //    {
+            //    Console.WriteLine(client.Remote.SessionID);
+            //        Task.Delay(1000).Wait();
+            //    }
+            //});
 
             Console.ReadKey();
 
