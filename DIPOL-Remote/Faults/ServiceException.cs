0eb5580b22a25e8a6325f60d8ee62555fbdad5dd
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-07-24 17:37:34 +0300

    Remote.Camera.ctor

--- DIPOL-Remote/Faults/ServiceException.cs
+++ DIPOL-Remote/Faults/ServiceException.cs
@@ -15,6 +15,10 @@ namespace DIPOL_Remote.Faults
         public static readonly FaultReason CameraCommunicationReason
             = new FaultReason($"Error occured while communicating with {nameof(ANDOR_CS.Classes.Camera)} object.");
 
+        public static readonly FaultReason CameraCommunicationReason
+            = new FaultReason($"Error occured while communicating with {nameof(ANDOR_CS.Classes.Camera)} object.");
+
+
         [DataMember(IsRequired = true, Order = 0)]
         public string Message;
         [DataMember(IsRequired = true, Order = 1)]
