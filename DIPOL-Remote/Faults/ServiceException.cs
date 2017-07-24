2ff3192b6632e90ef907f4278e417d742f87f4e3
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-07-24 15:06:51 +0300

    Proper dependencies; Integration with ANDOR classes

---
+++ DIPOL-Remote/Faults/ServiceException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.Serialization;
+
+namespace DIPOL_Remote.Faults
+{
+    [DataContract(IsReference = false)]
+    public class ServiceException
+    {
+        [DataMember(IsRequired = true, Order = 0)]
+        public string Message;
+        [DataMember(IsRequired = true, Order = 1)]
+        public string Details;
+        [DataMember(IsRequired = true, Order = 2)]
+        public string MethodName;
+    }
+}
--- DIPOL-Remote/RemoteControl.cs
+++ DIPOL-Remote/RemoteControl.cs
@@ -17,14 +17,25 @@
 
 using System;
 using System.Collections.Generic;
+using System.Collections.Concurrent;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 using System.ServiceModel;
 
+using DIPOL_Remote.Faults;
+
+using ANDOR_CS.Classes;
+using ANDOR_CS.Exceptions;
+
 namespace DIPOL_Remote
 {
+    /// <summary>
+    /// Implementation of <see cref="IRemoteControl"/> service interface.
+    /// This class should not be utilized directly.
+    /// Instances are executed on server (service) side.
+    /// </summary>
     [ServiceBehavior(
         ConcurrencyMode = ConcurrencyMode.Multiple, 
         InstanceContextMode = InstanceContextMode.PerSession,
@@ -32,20 +43,32 @@ namespace DIPOL_Remote
         IncludeExceptionDetailInFaults = true)]
     public class RemoteControl : IRemoteControl, IDisposable
     {
-        private string sessionID = "";
-        private bool IsDisposing = false;
+        /// <summary>
+        /// Unique ID of the current session
+        /// </summary>
+        private string sessionID = null;
+        /// <summary>
+        /// Operation context of current connection. Used for callbacks
+        /// </summary>
         private OperationContext context;
 
-        public static System.Collections.Concurrent.ConcurrentBag<RemoteControl> ServiceInstances = 
-            new System.Collections.Concurrent.ConcurrentBag<RemoteControl>();
+        /// <summary>
+        /// Thread-safe collection of all active <see cref="RemoteControl"/> service instances.
+        /// </summary>
+        private static ConcurrentDictionary<string, RemoteControl> serviceInstances 
+            = new ConcurrentDictionary<string, RemoteControl>();
 
-        public RemoteControl()
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        private RemoteControl()
         {
-            sessionID = Guid.NewGuid().ToString("N");
-            Console.WriteLine("Constructor invoked.");
-
+         
         }
                 
+        /// <summary>
+        /// Unique ID of current session
+        /// </summary>
         public string SessionID
         {
             [OperationBehavior]
@@ -53,31 +76,88 @@ namespace DIPOL_Remote
             
         }
 
+        /// <summary>
+        /// Interface to collection of all active <see cref="RemoteControl"/> service instances.
+        /// </summary>
+        public static IReadOnlyDictionary<string, RemoteControl> ActiveConnections
+        {
+            get => serviceInstances as IReadOnlyDictionary<string, RemoteControl>;
+        }
+
+        /// <summary>
+        /// Entry point of any connection.
+        /// </summary>
         [OperationBehavior]
         public void Connect()
         {
+            // Stores current context
             context = OperationContext.Current;
-            ServiceInstances.Add(this);
-            Console.WriteLine("Connection initialized.");
+            // Assigns session ID
+            sessionID = Guid.NewGuid().ToString("N");
+
+            int maxTry = 100;
+            int count = 0;
+            // Stores current instance of service class into collection
+            while(!serviceInstances.TryAdd(sessionID, this) & count++ < maxTry)
+                sessionID = Guid.NewGuid().ToString("N");
+
+            if (count >= maxTry)
+                throw new FaultException<ServiceException>(
+                    new ServiceException()
+                    {
+                        Message = "Initialization of connection failed.",
+                        Details = "Unable to generate unique session ID. " +
+                        "Failed to add current session to the pool of active sessions",
+                        MethodName = nameof(serviceInstances.TryAdd)
+                    }, 
+                    new FaultReason("Connection was not properly initialized.")
+                    );
+
         }
 
+        /// <summary>
+        /// Exit point for any connection. Frees resources.
+        /// </summary>
         [OperationBehavior]
         public void Disconnect()
         {
-            IsDisposing = true;
-            Dispose();
+           Dispose();
         }
-
+        
+        /// <summary>
+        /// Implementation of <see cref="IDisposable"/> interface. Frees resources.
+        /// </summary>
         public void Dispose()
         {
-            Console.WriteLine("In dispose");
-            if(IsDisposing)
-                Console.WriteLine("Disposing...");
+
         }
 
+        /// <summary>
+        /// Returns number of available cameras
+        /// </summary>
+        /// <returns></returns>
         [OperationBehavior]
         public int GetNumberOfCameras()
         {
+            try
+            {
+                throw new ArgumentException();
+            }
+            catch (AndorSDKException e1)
+            {
+                throw new FaultException<AndorSDKException>(e1);
+            }
+            catch (Exception e)
+            {
+                throw new FaultException<ServiceException>(
+                    new ServiceException()
+                    {
+                        Message = "Something happened",
+                        Details = e.Message
+                    },
+                    new FaultReason($"Error occured while communicating with {nameof(Camera)} class."));
+
+            }
             return 3;
         }
 
