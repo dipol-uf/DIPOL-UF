35e7c87ce7496c72f1bcf966558686b85875dc8f
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-07-24 19:40:51 +0300

    Dipol-Remote restructured

---
+++ DIPOL-Remote/Classes/DipolHost.cs
@@ -0,0 +1,53 @@
+﻿//    This file is part of Dipol-3 Camera Manager.
+
+//    Dipol-3 Camera Manager is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+
+//    Dipol-3 Camera Manager is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+
+//    You should have received a copy of the GNU General Public License
+//    along with Dipol-3 Camera Manager.  If not, see<http://www.gnu.org/licenses/>.
+//
+//    Copyright 2017, Ilia Kosenkov, Tuorla Observatory, Finland
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ServiceModel;
+
+using DIPOL_Remote.Interfaces;
+
+namespace DIPOL_Remote.Classes
+{
+    public class DipolHost : IDisposable
+    {
+        private static readonly Uri endpoint = new Uri(@"net.tcp://localhost:400/DipolRemote");
+
+        private ServiceHost host = null;
+
+        public DipolHost()
+        {
+            host = new ServiceHost(typeof(RemoteControl), endpoint);
+
+            host.AddServiceEndpoint(typeof(IRemoteControl), new NetTcpBinding(), "");
+        }
+
+        public void Host() => host?.Open();
+
+      
+        public void Dispose()
+        {
+            host?.Close();
+        }
+
+
+    }
+}
---
+++ DIPOL-Remote/Classes/RemoteCallbackHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ServiceModel;
+
+using DIPOL_Remote.Interfaces;
+
+namespace DIPOL_Remote.Classes
+{
+    [CallbackBehavior(ConcurrencyMode = ConcurrencyMode.Multiple,
+        IncludeExceptionDetailInFaults = true)]
+    class RemoteCallbackHandler : IRemoteCallback
+    {
+        public RemoteCallbackHandler()
+        {
+            Console.WriteLine("Remote control handler created");
+        }
+        public void SendToClient(string m)
+        {
+            Console.WriteLine($"From service \"{m}\"");
+        }
+    }
+}
