39afbb7d5f95d8233db562b44faf25ea73bc74c2
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-07-24 16:19:47 +0300

    Remote.GetNumberOfCameras

---
+++ DIPOL-Remote/Faults/AndorSDKServiceException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.Serialization;
+
+namespace DIPOL_Remote.Faults
+{
+    [DataContract(IsReference = false)]
+    public class AndorSDKServiceException : ServiceException
+    {
+        [DataMember(IsRequired = true)]
+        public uint ErrorCode;
+        
+    }
+}
