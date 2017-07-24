75f291bb616d891b67a6cc28479d36f82f05d3ef
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-07-24 16:19:47 +0300

    Remote.GetNumberOfCameras

--- DIPOL-Remote/Faults/ServiceException.cs
+++ DIPOL-Remote/Faults/ServiceException.cs
@@ -5,12 +5,16 @@ using System.Text;
 using System.Threading.Tasks;
 
 using System.Runtime.Serialization;
+using System.ServiceModel;
 
 namespace DIPOL_Remote.Faults
 {
     [DataContract(IsReference = false)]
     public class ServiceException
     {
+        public static readonly FaultReason CameraCommunicationReason
+            = new FaultReason($"Error occured while communicating with {nameof(ANDOR_CS.Classes.Camera)} object.");
+
         [DataMember(IsRequired = true, Order = 0)]
         public string Message;
         [DataMember(IsRequired = true, Order = 1)]
