a2134818f3e19c9f3144fa84fb8735fdda4f6f14
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-07-25 13:40:12 +0300

    RemoteCamera & RemoteControl Dispose()

--- DIPOL-Remote/Classes/CameraPropertiesPack.cs
+++ DIPOL-Remote/Classes/CameraPropertiesPack.cs
@@ -6,16 +6,26 @@ using System.Threading.Tasks;
 
 using ANDOR_CS.Enums;
 
+using System.Runtime.Serialization;
+
 namespace DIPOL_Remote.Classes
 {
+    [DataContract]
     public struct CameraPropertiesPack
     {
+        [DataMember(IsRequired = true)]
         bool IsActive;
+        [DataMember(IsRequired = true)]
         bool IsInitialized;
+        [DataMember(IsRequired = true)]
         string SerialNumber;
+        [DataMember(IsRequired = true)]
         string CameraModel;
+        [DataMember(IsRequired = true)]
         FanMode FanMode;
+        [DataMember(IsRequired = true)]
         Switch CoolerMode;
+        [DataMember(IsRequired = true)]
         int CameraIndex;
     }
 }
