40a344345e506f05ee84c077c0026c2fb3575cc1
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-07-26 12:17:58 +0300

    RemoteCamera property updates through callback

--- DIPOL-Remote/Faults/AndorSDKServiceException.cs
+++ DIPOL-Remote/Faults/AndorSDKServiceException.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.ServiceModel;
+
 using System.Runtime.Serialization;
 
 namespace DIPOL_Remote.Faults
@@ -11,6 +13,21 @@ namespace DIPOL_Remote.Faults
     [DataContract(IsReference = false)]
     public class AndorSDKServiceException : ServiceException
     {
+        public static FaultException<AndorSDKServiceException> WrapAndorSDKException(
+            ANDOR_CS.Exceptions.AndorSDKException e,
+            string method)
+            => new FaultException<AndorSDKServiceException>(
+                new AndorSDKServiceException()
+                {
+                    Message = "Andor SDK failed to execute operation.",
+                    Details = e.Message,
+                    ErrorCode = e.ErrorCode,
+                    MethodName = method
+                },
+                CameraCommunicationReason); 
+            
+
+
         [DataMember(IsRequired = true)]
         public uint ErrorCode;
         
