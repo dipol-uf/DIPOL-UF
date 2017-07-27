b737e88ec418f72f0f84780e5b86876fb44e1072
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-07-27 19:20:13 +0300

    CameraBase modified,, removed Interface dependence
    CameraBase now implements directly IDisposable and
    INotifyPropertyChanged and all ICameraControl properties/methods.
    ICameraControl is not used and kept for compatibility.

---
+++ Host/Host-SurfacePro3.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Host
+{
+    class Host
+    {
+        static void Main(string[] args)
+        {
+            var host = new DIPOL_Remote.DipolHost();
+            host.Host();
+
+            Console.ReadKey();
+            Console.WriteLine($"Service instances: {DIPOL_Remote.RemoteControl.ServiceInstances.Count}");
+            DIPOL_Remote.RemoteControl.ServiceInstances.First().SendToClient();
+            Console.ReadKey();
+            host.Dispose();
+        }
+    }
+}
