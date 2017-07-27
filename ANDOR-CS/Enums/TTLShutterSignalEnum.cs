b905739bae20f65c44a72d820e4a304c007a179b
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-07-27 19:35:58 +0300

    More service getters implemented

--- ANDOR-CS/Enums/TTLShutterSignalEnum.cs
+++ ANDOR-CS/Enums/TTLShutterSignalEnum.cs
@@ -1,14 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Runtime.Serialization;
 
 namespace ANDOR_CS.Enums
 {
+    [DataContract]
     public enum TTLShutterSignal : int
     {
+        [EnumMember]
         Low = 0,
+        [EnumMember]
         High = 1
     }
 }
