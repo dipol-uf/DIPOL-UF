4aa1eca38ba3c8f2ec525a63a6bcc9ad726d4eeb
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-08-16 15:08:12 +0300

    Update

--- Client/Client.cs
+++ Client/Client.cs
@@ -19,47 +19,56 @@ namespace Client
 
                 Console.WriteLine("Session ID {0}", client.SessionID);
 
-                Console.WriteLine($"Number of available cameras: {client.GetNumberOfCameras()}");
-
-                var cam1 = client.CreateRemoteCamera(0);
-                cam1.PropertyChanged += (sender, e) => Console.WriteLine($"Event {(sender as DIPOL_Remote.Classes.RemoteCamera).CameraIndex}\t{e.PropertyName}");
-                cam1.TemperatureStatusChecked += (sender, e) => Console.WriteLine($"Event{ (sender as DIPOL_Remote.Classes.RemoteCamera).CameraIndex}\t{e.EventTime}\t{e.Temperature}");
-                //var cam2 = client.CreateRemoteCamera(1);
-                cam1.GetCurrentTemperature();
-                cam1.FanControl(ANDOR_CS.Enums.FanMode.FullSpeed);
-                // cam2.FanControl(ANDOR_CS.Enums.FanMode.FullSpeed);
-                cam1.CoolerControl(ANDOR_CS.Enums.Switch.Disabled);
-                //cam2.SetTemperature(-12);
-                cam1.TemperatureMonitor(ANDOR_CS.Enums.Switch.Enabled, 100);
-
-                Console.WriteLine(cam1.CameraModel);
-                //Console.WriteLine(cam2.CameraModel);
-                Console.ReadKey();
-                cam1.SetTemperature(-5);
-                cam1.FanControl(ANDOR_CS.Enums.FanMode.FullSpeed);
-                cam1.CoolerControl(ANDOR_CS.Enums.Switch.Enabled);
-                
-                Console.ReadKey();
-                cam1.CoolerControl(ANDOR_CS.Enums.Switch.Disabled);
-                Console.ReadKey();
-                cam1.TemperatureMonitor(ANDOR_CS.Enums.Switch.Disabled);
-                //cam1.FanControl(ANDOR_CS.Enums.FanMode.FullSpeed);
-                Console.WriteLine(cam1.GetCurrentTemperature());
-                //Console.ReadKey();
-                //Console.WriteLine(client.ActiveRemoteCameras().Length);
-                //Console.WriteLine(cam2.CameraModel);
-               // cam2.Dispose();
-                //Console.WriteLine(client.ActiveRemoteCameras().Length);
-                //Console.WriteLine(cam1.CameraModel);
-                cam1.Dispose();
-                //Console.WriteLine(client.ActiveRemoteCameras().Length);
 
 
                 Console.ReadKey();
                 client.Disconnect();
 
             }
+
+            
         }
 
+        private static void TestCamera(DIPOL_Remote.Classes.DipolClient client)
+        {
+
+            Console.WriteLine($"Number of available cameras: {client.GetNumberOfCameras()}");
+
+            var cam1 = client.CreateRemoteCamera(0);
+            cam1.PropertyChanged += (sender, e) => Console.WriteLine($"Event {(sender as DIPOL_Remote.Classes.RemoteCamera).CameraIndex}\t{e.PropertyName}");
+            cam1.TemperatureStatusChecked += (sender, e) => Console.WriteLine($"Event{ (sender as DIPOL_Remote.Classes.RemoteCamera).CameraIndex}\t{e.EventTime}\t{e.Temperature}");
+            //var cam2 = client.CreateRemoteCamera(1);
+            cam1.GetCurrentTemperature();
+            cam1.FanControl(ANDOR_CS.Enums.FanMode.FullSpeed);
+            // cam2.FanControl(ANDOR_CS.Enums.FanMode.FullSpeed);
+            cam1.CoolerControl(ANDOR_CS.Enums.Switch.Disabled);
+            //cam2.SetTemperature(-12);
+            cam1.TemperatureMonitor(ANDOR_CS.Enums.Switch.Enabled, 100);
+
+            Console.WriteLine(cam1.CameraModel);
+            //Console.WriteLine(cam2.CameraModel);
+            Console.ReadKey();
+            cam1.SetTemperature(-5);
+            cam1.FanControl(ANDOR_CS.Enums.FanMode.FullSpeed);
+            cam1.CoolerControl(ANDOR_CS.Enums.Switch.Enabled);
+
+            Console.ReadKey();
+            cam1.CoolerControl(ANDOR_CS.Enums.Switch.Disabled);
+            Console.ReadKey();
+            cam1.TemperatureMonitor(ANDOR_CS.Enums.Switch.Disabled);
+            //cam1.FanControl(ANDOR_CS.Enums.FanMode.FullSpeed);
+            Console.WriteLine(cam1.GetCurrentTemperature());
+            //Console.ReadKey();
+            //Console.WriteLine(client.ActiveRemoteCameras().Length);
+            //Console.WriteLine(cam2.CameraModel);
+            // cam2.Dispose();
+            //Console.WriteLine(client.ActiveRemoteCameras().Length);
+            //Console.WriteLine(cam1.CameraModel);
+            cam1.Dispose();
+            //Console.WriteLine(client.ActiveRemoteCameras().Length);
+        }
+
+        private static void ResetHost(DIPOL_Remote.Classes.DipolClient client)
+        { }
     }
 }
