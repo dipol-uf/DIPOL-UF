f4045578906c993f20cb03500a7c8b399964c1c3
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-08-16 16:08:51 +0300

    Host events

--- Host/Host.cs
+++ Host/Host.cs
@@ -16,13 +16,11 @@ namespace Host
             using (var host = new DIPOL_Remote.Classes.DipolHost())
             {
                 host.Host();
-
-                Console.ReadKey();
+                host.EventReceived += (sender, message)
+                    => Console.WriteLine($"{((ANDOR_CS.Classes.CameraBase)sender).SerialNumber}: {message}");
+  
                 //Console.WriteLine($"Service instances: {DIPOL_Remote.Classes.RemoteControl.ActiveConnections.Count}");
-                if(DIPOL_Remote.Classes.RemoteControl.ActiveCameras.TryGetValue(0, out (string, ANDOR_CS.Classes.CameraBase) result))
-                    (result.Item2 as ANDOR_CS.Classes.DebugCamera).CameraModel = "123";
-                if(DIPOL_Remote.Classes.RemoteControl.ActiveCameras.TryGetValue(1, out result))
-                    (result.Item2 as ANDOR_CS.Classes.DebugCamera).CameraModel = "456";
+  
                 Console.ReadKey();
             }
         }
