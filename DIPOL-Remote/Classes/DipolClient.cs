fb1f97840bf57a255aa18f046673735c52d34b78
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-08-16 15:49:41 +0300

    DipolHost assigned to RemoteControl

--- DIPOL-Remote/Classes/DipolClient.cs
+++ DIPOL-Remote/Classes/DipolClient.cs
@@ -56,10 +56,7 @@ namespace DIPOL_Remote.Classes
         public void Disconnect()
             => Remote.Disconnect();
 
-        public void ResetHost()
-            => Remote.ResetHost();
-
-
+       
         public int GetNumberOfCameras()
             => Remote.GetNumberOfCameras();
 
