ffc9f80c18867db619a6e4f0abdd919628c0b632
Ilia <ilia.kosenkov.at.gm@gmail.com>
2017-08-22 17:37:09 +0300

    Event log support

--- Host/Host.cs
+++ Host/Host.cs
@@ -11,13 +11,26 @@ namespace Host
     {
         static void Main(string[] args)
         {
-
+            
 
             using (var host = new DIPOL_Remote.Classes.DipolHost())
             {
                 host.Host();
                 host.EventReceived += (sender, message)
-                    => Console.WriteLine($"{((ANDOR_CS.Classes.CameraBase)sender).SerialNumber}: {message}");
+                    =>
+                {
+                    if (!(sender is ANDOR_CS.Classes.DebugCamera))
+                    {
+                        string senderString = "";
+                        if (sender is ANDOR_CS.Classes.CameraBase cam)
+                            senderString = $"{cam.CameraModel}/{cam.SerialNumber}";
+                        else
+                            senderString = sender.ToString();
+
+                        Console.WriteLine($"[{{0,23:yyyy/MM/dd HH-mm-ss.fff}}] @ {senderString}: {message}", DateTime.Now);
+                    }
+
+                };
   
                 //Console.WriteLine($"Service instances: {DIPOL_Remote.Classes.RemoteControl.ActiveConnections.Count}");
   
