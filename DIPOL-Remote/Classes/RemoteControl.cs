7fe9c188854147be8ce3f22b0e3b5a4b51c907be
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-09-19 12:23:41 +0300

    Back to tuple

--- DIPOL-Remote/Classes/RemoteControl.cs
+++ DIPOL-Remote/Classes/RemoteControl.cs
@@ -555,10 +555,10 @@ namespace DIPOL_Remote.Classes
             => GetCameraSafe(sessionID, camIndex).Hardware;
 
         [OperationBehavior]
-        public ImageDisplayLib.Image PullNewImage(int camIndex)
+        public (byte[] Data, int Width, int Height) PullNewImage(int camIndex)
         {
             if (GetCameraSafe(sessionID, camIndex).AcquiredImages.TryDequeue(out ImageDisplayLib.Image im))
-                return im;
+                return (im.GetBytes(), im.Width, im.Height);
             else
                 throw new Exception();
 
