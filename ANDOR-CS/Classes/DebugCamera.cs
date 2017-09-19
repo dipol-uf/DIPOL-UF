ad9d3306d3ec1db8203e92292f70473abab9fcd5
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-09-19 20:40:48 +0300

    Attempt to reinvent Camera locks and test it offline

--- ANDOR-CS/Classes/DebugCamera.cs
+++ ANDOR-CS/Classes/DebugCamera.cs
@@ -17,17 +17,12 @@
 
 
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 using ANDOR_CS.Enums;
 using ANDOR_CS.DataStructures;
 using ANDOR_CS.Events;
 
-using CallerMemberNameAttribute = System.Runtime.CompilerServices.CallerMemberNameAttribute;
-using ANDOR_CS.Interfaces;
 using ImageDisplayLib;
 using System.Collections.Concurrent;
 
