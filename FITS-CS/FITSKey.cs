f46cc97efeed069e890f039725b2f37cc43bb1af
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-09-25 17:41:45 +0300

    FITSKey & FITSUnit

---
+++ FITS-CS/FITSKey.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FITS_CS
+{
+    public class FITSKey
+    {
+        public static readonly int KeySize = 80;
+        public static readonly int KeyHeaderSize = 8;
+
+        byte[] array = new byte[FITSKey.KeySize];
+
+        byte[] Data => array;
+
+        public FITSKey(byte[] data)
+        {
+            if (data == null)
+                throw new ArgumentNullException($"{nameof(data)} is null");
+            if (data.Length != KeySize)
+                throw new ArgumentException($"{nameof(data)} has wrong length");
+
+            Array.Copy(data, array, data.Length);
+        }
+
+        public static char[] IsFITSKey(byte[] data, int offset = 0)
+        {
+            if (data == null)
+                throw new ArgumentNullException($"{nameof(data)} is null");
+            if (data.Length <= KeySize)
+                throw new ArgumentException($"{nameof(data)} has wrong length");
+            if(offset + KeySize > data.Length)
+                throw new ArgumentException($"{nameof(data)} has wrong length");
+
+
+            return data
+                .Skip(offset)
+                .Take(KeyHeaderSize)
+                .Select(b => (char)b)
+                .Where(c => c!= ' ')
+                .ToArray();               
+            
+        }
+    }
+}
---
+++ FITS-CS/FITSUnit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FITS_CS
+{
+    public class FITSUnit
+    {
+        public static readonly int UnitSizeInBytes = 2880;
+
+        private byte[] array = new byte[FITSUnit.UnitSizeInBytes];
+
+        public byte[] Data => array;
+
+        public FITSUnit(byte[] data)
+        {
+            if (data == null)
+                throw new ArgumentNullException($"{nameof(data)} is null");
+            if (data.Length != UnitSizeInBytes)
+                throw new ArgumentException($"{nameof(data)} has wrong length");
+
+            Array.Copy(data, array, data.Length);
+        }
+
+        public char[][] IsKeywords
+            => Enumerable.Range(0, UnitSizeInBytes / FITSKey.KeySize)
+            .Select(i => FITSKey.IsFITSKey(Data, i * FITSKey.KeySize)).ToArray();
+    }
+}
