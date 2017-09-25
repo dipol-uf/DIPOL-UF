a0f2de6f6c5415d506f8c49acd12f129eba1365f
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-09-25 17:41:45 +0300

    FITSKey & FITSUnit

--- FITS-CS/FITSStream.cs
+++ FITS-CS/FITSStream.cs
@@ -7,9 +7,17 @@ using System.IO;
 
 namespace FITS_CS
 {
-    public class FITSStream : Stream
+    public class FITSStream : Stream, IDisposable
     {
         private Stream baseStream;
+
+        private int bytesConsumed = 0;
+
+        public bool IsDisposed
+        {
+            get;
+            private set;
+        } = false;
 
         public override bool CanRead => baseStream.CanRead;
 
@@ -41,5 +49,77 @@ namespace FITS_CS
 
         public override void Write(byte[] buffer, int offset, int count)
             => baseStream.Write(buffer, offset, count);
+        
+
+        public FITSStream(Stream str)
+            =>  baseStream = str ?? throw new ArgumentNullException($"{nameof(str)} is null");
+                    
+
+        public new void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (IsDisposed)
+                return;
+
+            if (disposing)
+                if (baseStream != null)
+                {
+                    baseStream.Close();
+                    baseStream.Dispose();
+                    IsDisposed = true;
+                }
+                        
+        }
+
+        public override void Close() => Dispose();
+
+        public FITSUnit ReadUnit()
+        {
+            if (IsDisposed)
+                throw new ObjectDisposedException("Stream is already disposed.");
+            if (!CanRead)
+                throw new NotSupportedException("Stream does not support reading.");
+
+            try
+            {
+                if (CanSeek && Position + FITSUnit.UnitSizeInBytes >= Length)
+                    throw new ArgumentException("Stream ended");
+                byte[] buffer = new byte[FITSUnit.UnitSizeInBytes];
+                baseStream.Read(buffer, 0, FITSUnit.UnitSizeInBytes);
+                bytesConsumed += FITSUnit.UnitSizeInBytes;
+                return new FITSUnit(buffer);
+            }
+            catch (ArgumentException ae)
+            {
+                throw new EndOfStreamException("Stream end was reached.", ae);
+            }
+            catch (IOException)
+            {
+                throw;
+            }
+            catch (Exception e)
+            {
+                throw new IOException("An unexpected error happened while reading the stream.", e);
+            }
+        }
+
+        public bool TryReadUnit(out FITSUnit unit)
+        {
+            unit = null;
+            try
+            {
+                unit = ReadUnit();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
     }
 }
