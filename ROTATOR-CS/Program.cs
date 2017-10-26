da410b0357d46ab6c9fa37f85e779090144707cb
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-10-26 22:39:00 +0300

    Reply endianess

--- ROTATOR-CS/Program.cs
+++ ROTATOR-CS/Program.cs
@@ -78,7 +78,8 @@ namespace ROTATOR_CS
                 rot.DataRecieved += (sender, e) 
                     => Console.WriteLine(new Reply(rot.LastRespond));
                 rot.SendCommand(Command.MoveToPosition, 2000);
-                
+
+                Console.ReadKey();
             }
         }
 
