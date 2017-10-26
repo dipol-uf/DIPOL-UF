2d6197ea8dc8dd5ce0e933879c38649c6198f2ca
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-10-26 22:47:46 +0300

    ReturnStatus created

---
+++ ROTATOR-CS/ReturnStatusEnum.cs
@@ -0,0 +1,25 @@
+﻿using System.Runtime.Serialization;
+
+namespace ROTATOR_CS
+{
+    [DataContract]
+    enum ReturnStatus : byte
+    {
+        [EnumMember]
+        Success = 100,
+        [EnumMember]
+        CommandLoadedIntoEEPROM = 101,
+        [EnumMember]
+        WrongChecksum = 1,
+        [EnumMember]
+        InvalidCommand = 2,
+        [EnumMember]
+        WrongType = 3,
+        [EnumMember]
+        InvalidValue = 4,
+        [EnumMember]
+        EEPROMLocked = 5,
+        [EnumMember]
+        CommandUnavailable = 6
+    }
+}
