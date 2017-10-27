dea0bf8c7e7f7534fdf8922bfd1fc6e4cf2b16fb
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-10-27 14:08:46 +0300

    Event system tested

--- ROTATOR-CS/Program.cs
+++ ROTATOR-CS/Program.cs
@@ -13,6 +13,8 @@ namespace ROTATOR_CS
     {
         static void Main(string[] args)
         {
+            System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US");
+
             Test2();
 
             Console.ReadKey();
@@ -26,49 +28,45 @@ namespace ROTATOR_CS
             foreach (var port in ports)
                 Console.WriteLine(port);
 
+            List<(int, double, double)> data = new List<(int, double, double)>();
+
             using (var rot = new Rotator("COM2"))
             {
-                rot.DataRecieved += (sender, e) =>
-                   Console.WriteLine(new Reply(rot.LastResponse));
-                 
-                rot.ErrorRecieved += (sender, e)
-                    => Console.WriteLine(new Reply(rot.LastResponse));
+                //rot.DataRecieved += (sender, e) =>
+                //   Console.WriteLine($"{ e.EventTime} {e.Reply}");
 
+                //rot.ErrorRecieved += (sender, e) =>
+                //    Console.WriteLine($"{ e.EventTime} {e.Reply}");
 
-                int N = 250;
+                for (int i = 1; i <= 50; i++)
+                {
+                    rot.SendCommand(Command.MoveToPosition, 0, (byte)CommandType.Absolute);
+                    rot.WaitResponse();
+                    rot.WaitPositionReached();
 
-                rot.SendCommand(Command.MoveToPosition, 0000000, (byte) CommandType.Absolute);
-                //System.Threading.Thread.Sleep(300);
+                    var t = System.Diagnostics.Stopwatch.StartNew();
 
-                //var t = System.Diagnostics.Stopwatch.StartNew();
+                    var angle = 2000;
+                    rot.SendCommand(Command.MoveToPosition, i * angle, (byte)CommandType.Absolute);
+                    rot.WaitResponse();
 
-                //for (int i = 0; i < N; i++)
-                //{
+                    rot.WaitPositionReached();
 
-                rot.WaitPositionReached();
-                Console.WriteLine("Position reached");
+                    t.Stop();
 
-                //for (int i = 0; i < N; i++)
-                //{
-                //    var statusDictionary = rot.GetStatus();
-                //    if (i % 10 == 0)
-                //        Console.Clear();
-                //    Console.WriteLine("----------------------------------");
-                //    foreach (var status in statusDictionary)
-                //        Console.WriteLine("{0,30}\t{1}", status.Key,  
-                //            (byte)status.Key < 8 ? status.Value.ToString() : (status.Value > 0).ToString());
-                //    //if (N % 100 == 0)
-                //    //    Console.Clear();
-                //    System.Threading.Thread.Sleep(150);
-                //    Console.CursorLeft = 0;
-                //    Console.CursorTop = 0;
-                //}
-                    //rot.WaitResponse();
-                //}
+                    data.Add((i * angle, t.ElapsedMilliseconds / 1000.0, i * angle * 1000.0 / t.ElapsedMilliseconds));
 
-                //t.Stop();
-                //Console.WriteLine("Total time: {0:F3}; Per rotation on {1} units: {2:e3}",
-                    //t.ElapsedMilliseconds / 1000.0, step, t.ElapsedMilliseconds / 1000.0 / N);
+                    Console.WriteLine($"Rotated on {i * angle} over {t.ElapsedMilliseconds / 1000.0} with average speed " +
+                        $"{(i * angle * 1000.0 / t.ElapsedMilliseconds).ToString("F1")} units per sec.");
+                }
+
+            }
+
+            using (var str = new StreamWriter("log.dat"))
+            {
+                str.WriteLine("{0, 16}{1,15}{2,20}", "Angle", "Time", "Avg.Speed");
+                foreach (var item in data)
+                    str.WriteLine("{0, 16}{1,15:F3}{2,20:F3}", item.Item1, item.Item2, item.Item3);
             }
         }
 
