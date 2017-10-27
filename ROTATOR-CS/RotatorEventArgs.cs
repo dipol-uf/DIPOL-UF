434f9fcc8ddacea2e8291e90200559ac43484228
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-10-27 14:08:30 +0300

    New event system

---
+++ ROTATOR-CS/RotatorEventArgs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace ROTATOR_CS
+{
+    [DataContract]
+    public class RotatorEventArgs : EventArgs
+    {
+        [DataMember]
+        public DateTime EventTime
+        {
+            get;
+            private set;
+        }
+
+        [DataMember]
+        public Reply Reply
+        {
+            get;
+            private set;
+        }
+
+        public RotatorEventArgs(byte[] message)
+        {
+            Reply = new Reply(message);
+
+            EventTime = DateTime.Now;
+        }
+
+
+    }
+}
