dc0602a8306d805de20a7de6008143fff7da675c
Ilia-Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-10-30 11:13:08 +0200

    Licensing

--- ROTATOR-CS/Reply.cs
+++ ROTATOR-CS/Reply.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿//    This file is part of Dipol-3 Camera Manager.
+
+//    Dipol-3 Camera Manager is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 3 of the License, or
+//    (at your option) any later version.
+
+//    Dipol-3 Camera Manager is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//    GNU General Public License for more details.
+
+//    You should have received a copy of the GNU General Public License
+//    along with Dipol-3 Camera Manager.  If not, see<http://www.gnu.org/licenses/>.
+//
+//    Copyright 2017, Ilia Kosenkov, Tuorla Observatory, Finland
+
+using System;
 using System.Runtime.Serialization;
 using System.Linq;
 
