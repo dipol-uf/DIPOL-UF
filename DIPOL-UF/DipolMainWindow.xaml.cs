796353b905120747b9bf3d6dd2b540f39fd3c034
Ilia <ilia.kosenkov.at.gm@gmail.com>
2017-10-31 16:13:06 +0200

    CleanUP and MVVM preparation

--- DIPOL-UF/DipolMainWindow.xaml.cs
+++ DIPOL-UF/DipolMainWindow.xaml.cs
@@ -14,10 +14,6 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 
-using DIPOL_UF.Windows;
-using DIPOL_UF.EventArgs;
-
-using ANDOR_CS.Classes;
 
 namespace DIPOL_UF
 {
@@ -26,51 +22,14 @@ namespace DIPOL_UF
     /// </summary>
     public partial class DipolMainWindow : Window
     {
-        private List<Camera> ConnectedLocalCameras = null;
-
-        public delegate void CameraEventHandler(object sender, System.EventArgs e);
-
-        public event CameraEventHandler LocalCameraLoaded;
-        
-        protected virtual void OnLocalCameraLoaded(Camera sender, CameraEventArgs e) => LocalCameraLoaded?.Invoke(sender, e);
-               
-        private void Window_Closed(object sender, System.EventArgs e)
-        {
-            if (ConnectedLocalCameras != null)
-                foreach (var cam in ConnectedLocalCameras)
-                    cam?.Dispose();
-
-            Application.Current.Shutdown();
-        }
-
-        private void DeviceControl_LocalDevices_Click(object sender, RoutedEventArgs e)
-        {
-            var loader = new CameraLoader();
-            loader.Owner = this;
-            loader.Topmost = true;
-            ConnectedLocalCameras = loader.ShowDialogAndWait()?.ToList();
-
-            if (ConnectedLocalCameras != null)
-                foreach (var newCam in ConnectedLocalCameras)
-                    OnLocalCameraLoaded(newCam, new CameraEventArgs());
-        }
-
-
-
+      
         public DipolMainWindow()
         {
             InitializeComponent();
-            LocalCameraLoaded += (s, e) =>
-             {
-                 if (s is Camera cam && cam.Capabilities.Features.HasFlag(ANDOR_CS.Enums.SDKFeatures.FanControl | ANDOR_CS.Enums.SDKFeatures.LowFanMode))
-                 {
-                     cam.FanControl(ANDOR_CS.Enums.FanMode.LowSpeed);
-                     Console.WriteLine($"Camera {cam.SerialNumber} fan slowed down.");
-                 }
-                 else
-                     Console.WriteLine($"Not suported.");
-
-             };
+            Loaded += (sender, e) => {
+                var wind = new Views.ProgressWindow();
+                wind.Show();
+            };
         }
     }
 }
