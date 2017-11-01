eb55f304e0a4a3d046284507a1d49476e5068767
Ilia Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-11-01 17:15:20 +0200

    An attempt to implement command binding

---
+++ DIPOL-UF/Commands/WindowDragCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Input;
+using System.Windows.Shapes;
+
+namespace DIPOL_UF.Commands
+{
+    class WindowDragCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+            => true;
+
+        public void Execute(object parameter)
+        {
+            return;
+        }
+    }
+}
