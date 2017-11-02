334c2c92920339b9b1a683d26597875294e8ad87
Ilia Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-11-02 12:31:39 +0200

    Window drag implemented as routed command

---
+++ DIPOL-UF/Commands/WindowDragCommand.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DIPOL_UF.Commands
+{
+    sealed class WindowDragCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public void Execute(object parameter)
+        {
+
+            if (parameter is Commands.EventCommandArgs<MouseButtonEventArgs> commandArgs)
+            {
+                var sender = commandArgs.Sender as FrameworkElement;
+                var parent = VisualTreeHelper.GetParent(sender);
+                while (!(parent is Window))
+                    parent = VisualTreeHelper.GetParent(parent);
+
+                if (parent is Window w)
+                {
+                    if(commandArgs.EventArgs.LeftButton== MouseButtonState.Pressed)
+                        w.DragMove();
+                }
+            }
+
+
+        }
+
+        public bool CanExecute(object parameter) => true;
+    }
+}
