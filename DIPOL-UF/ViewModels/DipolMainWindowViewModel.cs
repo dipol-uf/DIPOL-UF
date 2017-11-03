2c3bb4067f18d8e98514e8febc5fbebf3ab3cb70
Ilia Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-11-03 16:35:09 +0200

    ViewModels now derive from ViewModel<T>

--- DIPOL-UF/ViewModels/DipolMainWindowViewModel.cs
+++ DIPOL-UF/ViewModels/DipolMainWindowViewModel.cs
@@ -7,30 +7,22 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Windows.Input;
 using System.Windows;
+using DIPOL_UF.Models;
 
 namespace DIPOL_UF.ViewModels
 {
-    class DipolMainWindowViewMode : ObservableObject
+    class DipolMainWindowViewModel : ViewModel<Models.DipolMainWindow>
     {
-        private Models.DipolMainWindow model;
+
+        public DipolMainWindowViewModel(DipolMainWindow model) : base(model)
+        {
+        }
 
         public ICommand ConnectButtonCommand => model.ConnectButtonCommand as ICommand;
         public ICommand DisconnectButtonCommand => model.DisconnectButtonCommand as ICommand;
 
         public ObservableCollection<MenuItemViewModel> MenuBarItems 
             => model.MenuBarItems;
-
-
-        public DipolMainWindowViewMode(Models.DipolMainWindow model)
-        {
-            this.model = model ?? throw new ArgumentNullException();
-
-            model.PropertyChanged += ModelPropertyChanged;
-        }
-
-        protected virtual void ModelPropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            RaisePropertyChanged(e.PropertyName);
-        }
+            
     }
 }
