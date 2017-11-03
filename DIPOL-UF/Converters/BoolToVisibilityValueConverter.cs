6e76ac52dced350bc6be92a0f78304a30615b233
Ilia Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2017-11-03 10:47:25 +0200

    BoolToVisibility converter

---
+++ DIPOL-UF/Converters/BoolToVisibilityValueConverter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows;
+using System.Globalization;
+
+namespace DIPOL_UF.Converters
+{
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public class BoolToVisibilityValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var boolVal = (bool)value;
+            if (boolVal)
+                return Visibility.Visible;
+            else
+            {
+                if (parameter is Visibility vis)
+                    return vis;
+                else
+                    return Visibility.Hidden;
+            }
+                    
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var visVal = (Visibility)value;
+
+            switch (visVal)
+            {
+                case Visibility.Visible:
+                    return true;
+                case Visibility.Collapsed:
+                    if (parameter is bool boolPar)
+                        return boolPar;
+                    return false;
+                case Visibility.Hidden:
+                default:
+                    return false;
+            }
+        }
+    }
+}
