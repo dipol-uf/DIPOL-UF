7eb2a1480cd7dc2c68c087add02ed43ccdb289ce
Ilia <ilia.kosenkov.at.gm@gmail.com>
2017-11-06 12:15:25 +0200

    TreeView implementation started

---
+++ DIPOL-UF/Models/ConnectedCamerasTreeModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+using System.Windows;
+
+using ANDOR_CS.Classes;
+
+namespace DIPOL_UF.Models
+{
+    class ConnectedCamerasTreeModel : ObservableObject
+    {
+        private ObservableConcurrentDictionary<string, CameraBase> cameraList = new ObservableConcurrentDictionary<string, CameraBase>();
+        private string name = "";
+
+        public ObservableConcurrentDictionary<string, CameraBase> CameraList
+        {
+            get => cameraList;
+            set
+            {
+                if (value != cameraList)
+                {
+                    cameraList = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+        public string Name
+        {
+            get => name;
+            set
+            {
+                if (value != name)
+                {
+                    name = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+    }
+}
---
+++ DIPOL-UF/ViewModels/ConnectedCamerasTreeViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Windows.Input;
+using System.Windows;
+
+using ANDOR_CS.Classes;
+
+namespace DIPOL_UF.ViewModels
+{
+    class ConnectedCamerasTreeViewModel : ViewModel<Models.ConnectedCamerasTreeModel>
+    {
+
+        public ConnectedCamerasTreeViewModel(Models.ConnectedCamerasTreeModel model)
+            :base(model)
+        {
+        }
+
+        public string Name => model.Name;
+        public ObservableConcurrentDictionary<string, CameraBase> CameraList => model.CameraList;
+    }
+}
