86aa1ef5958f1801b5467960497b23e0e3466e5b
Ilia <ilia.kosenkov.at.gm@gmail.com>
2017-11-07 10:51:38 +0200

    New converter camera key str => loc name

---
+++ DIPOL-UF/Converters/CameraKeyToStringValueConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows;
+using System.Globalization;
+
+namespace DIPOL_UF.Converters
+{
+    [ValueConversion(typeof(string), typeof(string))]
+    class CameraKeyToStringValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string val = (value as string)?.Split(':')[0] ?? "Unknown";
+            if (val.ToLowerInvariant() == "localhost")
+                val = "Local";
+
+            return val + ":";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
