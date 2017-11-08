da1204aa56d81c5b645b0cb3e92f19c45c495b4c
Ilia <ilia.kosenkov.at.gm@gmail.com>
2017-11-08 17:04:25 +0200

    Temperature display

--- DIPOL-UF/Converters/CameraStatsToStringValueConverter.cs
+++ DIPOL-UF/Converters/CameraStatsToStringValueConverter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows.Data;
 using System.Windows;
 using System.Globalization;
@@ -9,7 +10,24 @@ namespace DIPOL_UF.Converters
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            return "123";
+            if (values.Length == 2 &&
+                values[0] is ObservableConcurrentDictionary<string, Dictionary<string, object>> statsCollection &&
+                values[1] is string &&
+                statsCollection.ContainsKey((string)values[1]))
+            {
+                Dictionary<string, object> cameraStats = statsCollection[(string)values[1]];
+
+                object value = parameter is string paramStr 
+                    && cameraStats != null
+                    && cameraStats.ContainsKey(paramStr)
+                    ? cameraStats[paramStr] 
+                    : null;
+                Helper.WriteLog(value?.ToString());
+
+                return value?.ToString() ?? "";
+            }
+            else
+                return "";
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
