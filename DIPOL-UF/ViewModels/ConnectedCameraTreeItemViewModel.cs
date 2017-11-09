53e2e2115b4c6273b35d21ca111cdacdeabcbe3d
Ilia <ilia.kosenkov.at.gm@gmail.com>
2017-11-09 15:58:14 +0200

    Tree item ViewModel

---
+++ DIPOL-UF/ViewModels/ConnectedCameraTreeItemViewModel.cs
@@ -0,0 +1,13 @@
+﻿
+namespace DIPOL_UF.ViewModels
+{
+    class ConnectedCameraTreeItemViewModel : ViewModel<Models.ConnectedCameraTreeItemModel>
+    {
+        public ANDOR_CS.Classes.CameraBase Camera => model.Camera;
+        public System.Collections.ObjectModel.ObservableCollection<MenuItemViewModel> ContextMenu => model.ContextMenu;
+
+        public ConnectedCameraTreeItemViewModel(Models.ConnectedCameraTreeItemModel model)
+            : base(model)
+        { }
+    }
+}
