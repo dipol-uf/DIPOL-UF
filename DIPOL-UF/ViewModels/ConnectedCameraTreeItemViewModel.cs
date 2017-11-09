53b7426625ba44dc508c7e7c7d0cbdbbee55a6ff
Ilia <ilia.kosenkov.at.gm@gmail.com>
2017-11-09 16:40:12 +0200

    Context menu test

--- DIPOL-UF/ViewModels/ConnectedCameraTreeItemViewModel.cs
+++ DIPOL-UF/ViewModels/ConnectedCameraTreeItemViewModel.cs
@@ -4,6 +4,7 @@
     {
         public ANDOR_CS.Classes.CameraBase Camera => model.Camera;
         public System.Collections.ObjectModel.ObservableCollection<MenuItemViewModel> ContextMenu => model.ContextMenu;
+        
 
         public ConnectedCameraTreeItemViewModel(Models.ConnectedCameraTreeItemModel model)
             : base(model)
