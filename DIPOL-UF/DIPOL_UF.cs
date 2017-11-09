e38e139406bfd53a5d6a5c7987e03b48f3860cbe
Ilia <ilia.kosenkov.at.gm@gmail.com>
2017-11-09 11:30:56 +0200

    Application-wide settings collection

--- DIPOL-UF/DIPOL_UF.cs
+++ DIPOL-UF/DIPOL_UF.cs
@@ -1,10 +1,19 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.IO;
 
 namespace DIPOL_UF
 {
     public static class DIPOL_UF_App
     {
+        private static readonly string coreConfigPath = "core.dipolconfig";
+
+        public static Dictionary<string, object> Settings
+        {
+            get;
+            private set;
+        }
+
         [STAThread]
         public static int Main(string[] args)
         {
@@ -13,7 +22,7 @@ namespace DIPOL_UF
 
             //TestSettingsWriter();
 
-            System.Diagnostics.Debug.Listeners.Add(new System.Diagnostics.TextWriterTraceListener(Console.Out));
+            //System.Diagnostics.Debug.Listeners.Add(new System.Diagnostics.TextWriterTraceListener(Console.Out));
             System.Diagnostics.Debug.AutoFlush = true;
 
             App applicationInstance = new App();
@@ -31,17 +40,35 @@ namespace DIPOL_UF
             return 0;
         }
 
+        static DIPOL_UF_App()
+        {
+            try
+            {
+                if (File.Exists(coreConfigPath))
+                    using (var str = new StreamReader(coreConfigPath))
+                        Settings = SettingsManager.Read(str);
+                else
+                    Settings = null;
+            }
+            catch (Exception e)
+            {
+                Settings = null;
+                Helper.WriteLog(e);
+            }
+        
+        }
+
         public static void TestSettingsWriter()
         {
             using (var str = new System.IO.StreamWriter("test.dipolconfig"))
             {
-                System.Collections.Generic.Dictionary<string, object> pars = new System.Collections.Generic.Dictionary<string, object>();
+                Dictionary<string, object> pars = new Dictionary<string, object>();
 
                 pars.Add("Settings1", true);
                 pars.Add("Settings2", 123);
                 pars.Add("Settings3", 123.1);
                 pars.Add("Settings4", "str");
-
+                pars.Add("RemoteLocations", new object[] { true, "dipol-2", "dipol-3", 213, "1,\t 2," });
                 SettingsManager.Write(str, pars);
 
 
