33a2745fd928c69fba717ae4983cbe2493be229e
Ilia <ilia.kosenkov.at.gm@gmail.com>
2017-11-13 10:50:50 +0200

    Camera properties viewmodel simplified

--- DIPOL-UF/ViewModels/CameraPropertiesViewModel.cs
+++ DIPOL-UF/ViewModels/CameraPropertiesViewModel.cs
@@ -13,58 +13,47 @@ namespace DIPOL_UF.ViewModels
 {
     class CameraPropertiesViewModel : ViewModel<CameraBase>
     {
+        private ObservableCollection<Tuple<string, string>> deviceCapabilities = new ObservableCollection<Tuple<string, string>>();
+        private ObservableCollection<Tuple<string, string>> deviceProperties = new ObservableCollection<Tuple<string, string>>();
+        private ObservableCollection<Tuple<string, string>> allProperties;
+
         public CameraBase Camera => model;
 
-        public ObservableCollection<Tuple<string, string>> AllProperties
-        {
-            get
-            {
-                return new ObservableCollection<Tuple<string, string>>( DeviceCapabilities.Concat(DeviceProperties));
-            }
-        }
+        public ObservableCollection<Tuple<string, string>> AllProperties => allProperties;
 
-        public ObservableCollection<Tuple<string, string>> DeviceCapabilities
-        {
-            get
-            {
-                var collection = new ObservableCollection<Tuple<string, string>>();
-                var type = model.Capabilities.GetType();
-                var propertyList = type.GetProperties(BindingFlags.Instance | BindingFlags.Public);
 
-                foreach (var item in propertyList)
-                    collection.Add(new Tuple<string, string>(
-                        item.Name,
-                        item.GetValue(model.Capabilities)?.ToString() ?? "Unknown"));
-
-                return collection;
-
-            }
-        }
-
-        public ObservableCollection<Tuple<string, string>> DeviceProperties
-        {
-            get
-            {
-                var collection = new ObservableCollection<Tuple<string, string>>();
-                var type = model.Properties.GetType();
-                var propertyList = type.GetProperties(BindingFlags.Instance | BindingFlags.Public);
-
-                foreach (var item in propertyList)
-                {
-                    var value = item.GetValue(model.Properties);
-                    if (value is Array a)
-                        collection.Add(new Tuple<string, string>(item.Name, a.ArrayToString()));
-                    else
-                        collection.Add(new Tuple<string, string>(item.Name, value.ToString()));
-                }
-
-                    return collection;
-            }
-        }
-
-        public CameraPropertiesViewModel(CameraBase model) 
+        public CameraPropertiesViewModel(CameraBase model)
             : base(model)
         {
+            var type = model.Capabilities.GetType();
+            var propertyList = type.GetProperties(BindingFlags.Instance | BindingFlags.Public);
+
+            foreach (var item in propertyList)
+                deviceCapabilities.Add(new Tuple<string, string>(
+                    item.Name,
+                    item.GetValue(model.Capabilities)?.ToString() ?? "Unknown"));
+
+            type = model.Properties.GetType();
+            propertyList = type.GetProperties(BindingFlags.Instance | BindingFlags.Public);
+
+            foreach (var item in propertyList)
+            {
+                var value = item.GetValue(model.Properties);
+                if (value is Array a)
+                    deviceProperties.Add(new Tuple<string, string>(item.Name, a.ArrayToString()));
+                else
+                    deviceProperties.Add(new Tuple<string, string>(item.Name, value.ToString()));
+            }
+
+
+            allProperties = new ObservableCollection<Tuple<string, string>>(
+                (new[] {
+                    new Tuple<string, string>("Camera Model", model.CameraModel) ,
+                    new Tuple<string, string>("Serial Number", model.SerialNumber),
+                    new Tuple<string, string>("Software Version", model.Software.ToString()),
+                    new Tuple<string, string>("Hardware Version", model.Hardware.ToString())})
+                .Concat(deviceCapabilities).Concat(deviceProperties));
+
         }
     }
 }
