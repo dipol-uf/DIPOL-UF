5bbe00038968cd3437b9ce7c27d07495e7723349
Ilia <ilia.kosenkov.at.gm@gmail.com>
2017-11-24 15:16:07 +0200

    Connected camera model/viewmodel

---
+++ DIPOL-UF/Models/ConnectedCamera.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ANDOR_CS.Classes;
+
+namespace DIPOL_UF.Models
+{
+    class ConnectedCamera : ObservableObject
+    {
+        private CameraBase camera = null;
+
+
+
+        public CameraBase Camera
+        {
+            get => camera;
+            private set
+            {
+                if (value != camera)
+                {
+                    camera = value;
+                    RaisePropertyChanged();
+                }
+            }
+        }
+
+
+        public ConnectedCamera(CameraBase camera)
+        {
+            Camera = camera;
+        }
+    }
+}
---
+++ DIPOL-UF/ViewModels/ConnectedCameraViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DIPOL_UF.Models;
+
+using ANDOR_CS.Classes;
+
+namespace DIPOL_UF.ViewModels
+{
+    class ConnectedCameraViewModel : ViewModel<ConnectedCamera>
+    {
+        public CameraBase Camera => model.Camera;
+
+        
+        public ConnectedCameraViewModel(ConnectedCamera model) : base(model)
+        {
+            
+        }
+    }
+}
