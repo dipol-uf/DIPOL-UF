7e923ba4b0af9b74a4795ee68d4c036be309c04b
Ilia <ilia.kosenkov.at.gm@gmail.com>
2017-11-24 16:45:23 +0200

    Temperature controls on the camera tab

--- DIPOL-UF/Converters/CameraStatsToStringMultiValueConverter.cs
+++ DIPOL-UF/Converters/CameraStatsToStringMultiValueConverter.cs
@@ -10,33 +10,14 @@ namespace DIPOL_UF.Converters
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            var value = ConvertWorker(values, targetType, parameter, culture);
+            var value = new CameraStatsToValueMultiValueConverter().ConvertWorker(values, targetType, parameter, culture);
 
             if (value is float)
                 return ((float)value).ToString("#0.00");
             else return value?.ToString() ?? "";
         }
 
-        internal object ConvertWorker(object[] values, Type targetType, object parameter, CultureInfo culture)
-        {
-            if (values.Length == 2 &&
-                values[0] is ObservableConcurrentDictionary<string, Dictionary<string, object>> statsCollection &&
-                values[1] is string &&
-                statsCollection.ContainsKey((string)values[1]))
-            {
-                Dictionary<string, object> cameraStats = statsCollection[(string)values[1]];
-
-                object value = parameter is string paramStr
-                    && cameraStats != null
-                    && cameraStats.ContainsKey(paramStr)
-                    ? cameraStats[paramStr]
-                    : null;
-
-                return value ?? null;
-            }
-            else
-                return null;
-        }
+       
 
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
