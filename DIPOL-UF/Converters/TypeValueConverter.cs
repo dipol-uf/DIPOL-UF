714d07ed8187c93a18793d57bcec79d29c12a999
Ilia <ilia.kosenkov.at.gm@gmail.com>
2017-11-27 17:13:20 +0200

    Camera Tab improvements

---
+++ DIPOL-UF/Converters/TypeValueConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows;
+using System.Globalization;
+
+namespace DIPOL_UF.Converters
+{
+    class TypeValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+                return System.Convert.ChangeType(value, targetType);
+            else if (targetType.IsValueType)
+                return Activator.CreateInstance(targetType);
+            else return null;
+
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+                return System.Convert.ChangeType(value, targetType);
+            else if (targetType.IsValueType)
+                return Activator.CreateInstance(targetType);
+            else return null; 
+        }
+    }
+}
