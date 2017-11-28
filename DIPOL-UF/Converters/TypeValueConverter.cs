217682a1c59d25cf7bd1758c81f4ae7e6dbff8b7
Ilia <ilia.kosenkov.at.gm@gmail.com>
2017-11-28 11:05:36 +0200

    Converters rearranged

--- DIPOL-UF/Converters/TypeValueConverter.cs
+++ DIPOL-UF/Converters/TypeValueConverter.cs
@@ -5,6 +5,10 @@ using System.Globalization;
 
 namespace DIPOL_UF.Converters
 {
+    /// <summary>
+    /// Performs basic cast operations between types.
+    /// Utilizes <see cref="System.Convert"/>
+    /// </summary>
     class TypeValueConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
@@ -17,12 +21,23 @@ namespace DIPOL_UF.Converters
 
         }
 
+        /// <summary>
+        /// Converts back. Works when binding is TwoWay or OneWayToSource.
+        /// </summary>
+        /// <param name="value">Value to convert.</param>
+        /// <param name="targetType">Type.</param>
+        /// <param name="parameter">Optional parameter.</param>
+        /// <param name="culture">Culture information.</param>
+        /// <returns></returns>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
+            // If val is nor null
             if (value != null)
                 return System.Convert.ChangeType(value, targetType);
+            // If it is null and it is a value type, create a default(targetType)
             else if (targetType.IsValueType)
                 return Activator.CreateInstance(targetType);
+            // Otherwise it is a ref type and simply return null
             else return null; 
         }
     }
