284f0417b37b30173af57bf3737a159899346ec3
Ilia <ilia.kosenkov.at.gm@gmail.com>
2017-11-29 10:46:03 +0200

    DescriptionAttribute added to Enum

--- ANDOR-CS/Enums/ShutterModeEnum.cs
+++ ANDOR-CS/Enums/ShutterModeEnum.cs
@@ -1,18 +1,24 @@
 ﻿using System.Runtime.Serialization;
+using System.ComponentModel;
 
 namespace ANDOR_CS.Enums
 {
    [DataContract]
     public enum ShutterMode : int
     {
+        [Description("Fully Automatic")]
         [EnumMember]
         FullyAuto = 0,
+        [Description("Permanently Open")]
         [EnumMember]
         PermanentlyOpen = 1,
+        [Description("Permanently Closed")]
         [EnumMember]
         PermanentlyClosed = 2,
+        [Description("Open for Full Vertical Binning")]
         [EnumMember]
         OpenForFVBSeries = 4,
+        [Description("Open for Any Series")]
         [EnumMember]
         OpenForAnySeries = 5
     }
