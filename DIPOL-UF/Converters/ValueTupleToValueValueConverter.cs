1b3642743f8af48dca1cc906a8810fb0b7de08d1
Ilia <ilia.kosenkov.at.gm@gmail.com>
2017-11-30 13:59:22 +0200

    ValueType field accessor converter

---
+++ DIPOL-UF/Converters/ValueTupleToValueValueConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Data;
+using System.Reflection;
+using System.Globalization;
+
+
+namespace DIPOL_UF.Converters
+{
+    class ValueTupleToValueValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var type = value.GetType();
+            if (type.IsValueType && parameter is string fieldName)
+                return type
+                    .GetField(fieldName, BindingFlags.Public | BindingFlags.Instance)
+                    ?.GetValue(value);
+            else return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException("Backward convertion is not supported");
+        }
+    }
+}
