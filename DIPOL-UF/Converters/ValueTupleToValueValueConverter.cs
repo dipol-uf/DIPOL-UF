5920132c883705f96d7bc7102422fea0b37de147
Ilia <ilia.kosenkov.at.gm@gmail.com>
2017-11-30 17:37:32 +0200

    ValueTuple Converter now accepts arrays of tuples

--- DIPOL-UF/Converters/ValueTupleToValueValueConverter.cs
+++ DIPOL-UF/Converters/ValueTupleToValueValueConverter.cs
@@ -11,11 +11,32 @@ namespace DIPOL_UF.Converters
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             var type = value.GetType();
-            if (type.IsValueType && parameter is string fieldName)
-                return type
-                    .GetField(fieldName, BindingFlags.Public | BindingFlags.Instance)
-                    ?.GetValue(value);
-            else return null;
+            if (parameter is string fieldName)
+            {
+                if (type.BaseType == typeof(Array))
+                {
+                    var array = value as Array;
+                    Type innerType;
+                    FieldInfo fi;
+                    if (array.Length > 0 &&
+                        (innerType = array.GetValue(0).GetType()).IsValueType &&
+                        (fi = innerType.GetField(fieldName, BindingFlags.Public | BindingFlags.Instance)) != null)
+                    {
+                        Array result = Array.CreateInstance(fi.FieldType, array.Length);
+                        for (int i = 0; i < array.Length; i++)
+                            result.SetValue(fi.GetValue(array.GetValue(i)), i);
+
+                        return result;
+                    }
+                }
+                else if (type.IsValueType)
+                    return type
+                        .GetField(fieldName, BindingFlags.Public | BindingFlags.Instance)
+                        ?.GetValue(value);
+
+                
+            }
+            return null;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
