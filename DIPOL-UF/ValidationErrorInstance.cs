6e055393b46028603bc66ab88d55330011e9465c
Ilia <ilia.kosenkov.at.gm@gmail.com>
2017-12-04 16:47:24 +0200

    Improved validation messages

---
+++ DIPOL-UF/ValidationErrorInstance.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIPOL_UF
+{
+    class ValidationErrorInstance  : IEquatable<ValidationErrorInstance>
+    {
+       
+        public string Message
+        {
+            get;
+            private set;
+
+        }
+
+        public string ID
+        {
+            get;
+            private set;
+        }
+
+        public ValidationErrorInstance(string id, string message)
+        {
+            Message = message;
+            ID = id;
+        }
+
+        public bool Equals(ValidationErrorInstance other)
+            => this.ID == other.ID;
+
+        public override bool Equals(object obj)
+            => obj is ValidationErrorInstance inst ? inst.ID == this.ID : false;
+
+        public override int GetHashCode()
+            => this.ID.GetHashCode();
+        public override string ToString()
+            => ID;
+    }
+}
