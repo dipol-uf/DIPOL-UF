147e04deeb1d00f05fd94e57c4af2b432403faff
Ilia <ilia.kosenkov.at.gm@gmail.com>
2018-02-27 18:18:16 +0200

    Json format tested locally

--- Tests/DebuggingTests.cs
+++ Tests/DebuggingTests.cs
@@ -6,8 +6,6 @@ using System.Windows;
 using System.Windows.Threading;
 using DipolImage;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using OxyPlot;
-using OxyPlot.Series;
 
 
 namespace Tests
@@ -17,7 +15,7 @@ namespace Tests
         [STAThread]
         public static int Main()
         {
-            new Debugger().DisplayImage();
+            new Debugger();
             return 0;
         }
 
@@ -35,46 +33,6 @@ namespace Tests
                 //TestImageUInt16 = new Image(initArr, 512, 256);
             }
 
-            public void DisplayImage()
-            {
-
-                List<DataPoint> points = new List<DataPoint>();
-
-                var r = new Random();
-
-                var data = Enumerable.Range(0, 10000)
-                                     .Select(i => Math.Pow(1e3*i, 2)*Math.Exp(-i*1e-16) * r.NextDouble())
-                                     .ToList();
-
-                var min = data.Min();
-                var max = data.Max();
-                var N = 1000;
-
-                var dx = (max - min) / N;
-                for (var i = 0; i < N; i++)
-                {
-                    var count = data.Count(x => x > (min + i * dx) && x < (min + (i + 1) * dx));
-                    points.Add(new DataPoint(min + dx * (i+0.5), count));
-                }
-
-
-                var app = new Application();
-                var wind = new Tests.DebugWindow()
-                {
-                    DataContext = points
-                };
-                DispatcherTimer t = new DispatcherTimer()
-                {
-                    Interval = TimeSpan.FromSeconds(10)
-                };
-
-              
-                t.Start();
-              
-                app.Run(wind);
-
-
-            }
         }
     }
 }
