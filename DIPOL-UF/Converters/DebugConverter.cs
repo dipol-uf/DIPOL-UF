d10d98e609a23fb4b38669015ab9a843913331ac
Ilia <ilia.kosenkov.at.gm@gmail.com>
2018-03-06 16:14:35 +0200

    Debug empty converter

---
+++ DIPOL-UF/Converters/DebugConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace DIPOL_UF.Converters
+{
+    class DebugConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
+
