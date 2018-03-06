5d2f75fd7b69a83ceb6091a7079f94bc9d5370ed
Ilia <ilia.kosenkov.at.gm@gmail.com>
2018-03-06 16:15:06 +0200

    Default converter that returns [parameter] if [value] is null

---
+++ DIPOL-UF/Converters/DefaultValueConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace DIPOL_UF.Converters
+{
+    class DefaultValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var isNullVal = (value == null) || (value is string s && string.IsNullOrEmpty(s));
+
+            if (!isNullVal)
+                return value;
+            else if (parameter != null)
+                return parameter;
+            else
+                return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
