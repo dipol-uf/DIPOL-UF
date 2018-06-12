0825dfe76012dabb0f0a797217d612e55f5a1e02
Ilia <ilia.kosenkov.at.gm@gmail.com>
2018-06-12 16:07:58 +0300

    Testing

---
+++ Tests/RemoteCommunicationTestscs.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DIPOL_Remote.Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class RemoteCommunicationTestscs
+    {
+        [TestMethod]
+#if X86
+        [DeploymentItem("atmcd32d.dll")]
+#endif
+#if X64
+        [DeploymentItem("atmcd64d.dll")]
+#endif
+        public void Test_CameraCreationRequest()
+        {
+            using (var client = new DipolClient(@"localhost"))
+            {
+
+            }
+        }
+    }
+}
