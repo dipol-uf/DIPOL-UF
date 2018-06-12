5a1d9a17ac73d026c339c17f2afd78dbb43bfcfe
Ilia <ilia.kosenkov.at.gm@gmail.com>
2018-06-12 09:21:27 +0300

    Debugging camera connection

--- Host/Host.cs
+++ Host/Host.cs
@@ -16,39 +16,50 @@ namespace Host
             Console.WindowWidth = 180;
             Console.WindowHeight = 60;
 
-            using (var host = new DIPOL_Remote.Classes.DipolHost())
+            Debug();
+
+            //using (var host = new DIPOL_Remote.Classes.DipolHost())
+            //{
+            //    host.Host();
+            //    host.EventReceived += (sender, message)
+            //        =>
+            //    {
+            //        if (!(sender is ANDOR_CS.Classes.DebugCamera))
+            //        {
+            //            string senderString = "";
+            //            if (sender is ANDOR_CS.Classes.CameraBase cam)
+            //                senderString = $"{cam.CameraModel}/{cam.SerialNumber}";
+            //            else
+            //                senderString = sender.ToString();
+
+            //            lock (locker)
+            //            {
+            //                Console.ForegroundColor = ConsoleColor.Yellow;
+            //                Console.Write($"[{{0,23:yyyy/MM/dd HH-mm-ss.fff}}] @", DateTime.Now);
+            //                Console.ForegroundColor = ConsoleColor.Cyan;
+            //                Console.Write(" {0, 16}", senderString);
+            //                Console.ForegroundColor = ConsoleColor.White;
+            //                Console.WriteLine($": { message}");
+            //            }
+            //        }
+
+            //    };
+
+            //    //Console.WriteLine($"Service instances: {DIPOL_Remote.Classes.RemoteControl.ActiveConnections.Count}");
+
+            //    ConsoleKeyInfo key = default(ConsoleKeyInfo);
+
+            //    while (!((key = Console.ReadKey()).Key == ConsoleKey.Escape && key.Modifiers == ConsoleModifiers.Shift))
+            //    { }
+            //}
+        }
+
+        private static void Debug()
+        {
+            using(var cam = new ANDOR_CS.Classes.Camera())
             {
-                host.Host();
-                host.EventReceived += (sender, message)
-                    =>
-                {
-                    if (!(sender is ANDOR_CS.Classes.DebugCamera))
-                    {
-                        string senderString = "";
-                        if (sender is ANDOR_CS.Classes.CameraBase cam)
-                            senderString = $"{cam.CameraModel}/{cam.SerialNumber}";
-                        else
-                            senderString = sender.ToString();
-
-                        lock (locker)
-                        {
-                            Console.ForegroundColor = ConsoleColor.Yellow;
-                            Console.Write($"[{{0,23:yyyy/MM/dd HH-mm-ss.fff}}] @", DateTime.Now);
-                            Console.ForegroundColor = ConsoleColor.Cyan;
-                            Console.Write(" {0, 16}", senderString);
-                            Console.ForegroundColor = ConsoleColor.White;
-                            Console.WriteLine($": { message}");
-                        }
-                    }
-
-                };
-
-                //Console.WriteLine($"Service instances: {DIPOL_Remote.Classes.RemoteControl.ActiveConnections.Count}");
-
-                ConsoleKeyInfo key = default(ConsoleKeyInfo);
-
-                while (!((key = Console.ReadKey()).Key == ConsoleKey.Escape && key.Modifiers == ConsoleModifiers.Shift))
-                { }
+                Console.WriteLine(cam.CameraModel);
+                Console.ReadKey();
             }
         }
     }
