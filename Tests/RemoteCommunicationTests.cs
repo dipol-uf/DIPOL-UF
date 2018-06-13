80bdf0df852efd6f1b263b83f83d031f1d0505dc
Ilia <ilia.kosenkov.at.gm@gmail.com>
2018-06-13 15:24:52 +0300

    TestClass cleared

--- Tests/RemoteCommunicationTests.cs
+++ Tests/RemoteCommunicationTests.cs
@@ -12,61 +12,6 @@ namespace Tests
     [TestClass]
     public class RemoteCommunicationTests
     {
-        [TestMethod]
-#if X86
-        [DeploymentItem("atmcd32d.dll")]
-#endif
-#if X64
-        [DeploymentItem("atmcd64d.dll")]
-#endif
-        public void Test_CameraCreationRequest()
-        {
-            using (var client = new DipolClient(@"dipol-2"))
-            {
-                client.Connect();
 
-                int nCam = client.GetNumberOfCameras();
-                if (nCam == 0)
-                {
-
-                    Assert.ThrowsException<System.ServiceModel.FaultException<AndorSDKServiceException>>(() =>
-                        RemoteCamera.Create(0, client));
-
-                }
-                else
-                {
-                    var cam = RemoteCamera.Create(0, client);
-                    Assert.IsNotNull(cam);
-                    cam.Dispose();
-                    Assert.IsTrue(cam.IsDisposed);
-                }
-
-                client.Disconnect();
-
-            }
-        }
-
-        [TestMethod]
-#if X86
-        [DeploymentItem("atmcd32d.dll")]
-#endif
-#if X64
-        [DeploymentItem("atmcd64d.dll")]
-#endif
-        public void Debug()
-        {
-            using (var client = new DipolClient(@"dipol-2"))
-            {
-                client.Connect();
-
-                var cam = RemoteCamera.CreateAsync(0, client).Result;
-
-                cam?.Dispose();
-                //System.Threading.Thread.Sleep(TimeSpan.FromSeconds(25));
-                //System.Threading.Thread.Sleep(TimeSpan.FromSeconds(10));
-
-                client.Disconnect();
-            }
-        }
     }
 }
