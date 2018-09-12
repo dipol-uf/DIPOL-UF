cf20bbc7b32aca06350c685c1db052661b0c8502
Ilia <ilia.kosenkov.at.gm@gmail.com>
2018-09-12 11:14:55 +0300

    DeployItem now supports target paths

--- Tests/DeployItemAttribute.cs
+++ Tests/DeployItemAttribute.cs
@@ -1,46 +1,56 @@
 ﻿using System;
 using System.IO;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using NUnit.Framework;
+using NUnit.Framework.Constraints;
 using NUnit.Framework.Interfaces;
 using NUnit.Framework.Internal;
 
 namespace Tests
 {
-    public class DeployItemAttribute : Attribute, IApplyToTest
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class DeployItemAttribute : NUnitAttribute, IApplyToTest
     {
         private readonly string _copyFrom;
-        private readonly string CopyFrom;
+        private readonly string _copyTo = null;
 
-        public DeployItemAttribute(string item) =>
-            _copyFrom = item;
+        public DeployItemAttribute(string from) =>
+            _copyFrom = from ?? throw new ArgumentNullException(nameof(from));
+
+        public DeployItemAttribute(string from, string to)
+        {
+            _copyFrom = from ?? throw new ArgumentNullException(nameof(from));
+            _copyTo = to;
+        }
 
         public void ApplyToTest(Test test)
         {
+
+            string CollapsePath(string input) =>
+                Path.GetFullPath(
+                    Path.IsPathRooted(input)
+                        ? input
+                        : Path.Combine(TestContext.CurrentContext.TestDirectory, input));
+
             try
             {
-                
-                var copyFrom = Path.GetFullPath(
-                    Path.IsPathRooted(_copyFrom)
-                        ? _copyFrom
-                        : Path.Combine(TestContext.CurrentContext.TestDirectory, _copyFrom));
+                var copyFrom = CollapsePath(_copyFrom);
 
                 if (!File.Exists(copyFrom))
                     throw new FileNotFoundException($"File {copyFrom} cannot be found.");
 
-                var shortName = Path.GetFileName(copyFrom);
+                var copyTo = CollapsePath(_copyTo ?? Path.GetFileName(_copyFrom));
 
-                var dest = Path.GetFullPath(Path.Combine(TestContext.CurrentContext.TestDirectory, shortName));
-
-                if (!File.Exists(dest) ||
-                    File.GetLastAccessTimeUtc(copyFrom) > File.GetLastAccessTimeUtc(dest))
+                if (!File.Exists(copyTo) ||
+                    File.GetLastAccessTimeUtc(copyFrom) > File.GetLastAccessTimeUtc(copyTo))
                 {
-                    File.Copy(_copyFrom, dest);
+                    File.Copy(copyFrom, copyTo, true);
                 }
             }
             catch(Exception e)
             {
-                test.MakeInvalid($"Failed to prepare test environment. Reason: \"{e.Message}\"");
+                test.MakeInvalid($"Failed to prepare test environment. Reason: \"{e.Message}\".");
             }
         }
     }
