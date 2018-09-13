7d3dcb3db4b35da38d1dee8bf05619529e1c203d
Ilia Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2018-09-14 01:06:08 +0300

    Read/Write tests of FITS image

--- Tests/FitsTests.cs
+++ Tests/FitsTests.cs
@@ -23,7 +23,9 @@
 //     SOFTWARE.
 
 using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using DipolImage;
 using FITS_CS;
@@ -31,35 +33,128 @@ using NUnit.Framework;
 
 namespace Tests
 {
+
+    public class FitsTestsData
+    {
+        // ReSharper disable once InconsistentNaming
+        public static IEnumerable Test_ReadWriteData
+        {
+            get
+            {
+                yield return new TestCaseData(4, 8);
+                yield return new TestCaseData(8, 4);
+                yield return new TestCaseData(256, 1);
+                yield return new TestCaseData(1, 256);
+                yield return new TestCaseData(431, 197);
+                yield return new TestCaseData(512, 512);
+                yield return new TestCaseData(1024, 1024);
+
+            }
+        }
+    }
     [TestFixture]
     public class FitsTests
     {
-        private List<double> _testData;
+        private readonly List<Action> _cleanupActions = new List<Action>();
 
-        [SetUp]
-        public void Initialize()
+        [TearDown]
+        public void TearDown()
         {
-            var r = new Random();
+            foreach (var action in _cleanupActions)
+            {
+                action();
+            }
 
-            _testData = Enumerable.Range(0, 1024 * 512)
-                      .Select((i) => ((100 * Math.Sqrt(i % 256) + r.Next(0, 512))))
-                      .ToList();
+            _cleanupActions.Clear();
+        }
+
+        [Theory]
+        [TestCaseSource(typeof(FitsTestsData), nameof(FitsTestsData.Test_ReadWriteData))]
+        [Parallelizable(ParallelScope.Self)]
+        public void Test_ReadWrite(int  width, int height)
+        {
+
+            var testData = Enumerable.Range(0, width * height)
+                                 .Select(i => 32 * i)
+                                 .ToList();
+
+            void GenerateAssert<T>(string path, IEnumerable compareTo) where T: struct
+            {
+                using (var str = new FITSStream(new FileStream(GetPath(path), FileMode.Open)))
+                {
+                    var data = new List<T>(width * height);
+                    while (str.TryReadUnit(out var unit))
+                    {
+                        if (unit.IsData)
+                            data.AddRange(unit.GetData<T>());
+                    }
+                    Assert.That(data.Take(width * height), Is.EqualTo(compareTo).AsCollection);
+                }
+            }
+
+            var file = $"test_dbl_{width:0000}x{height:0000}.fits";
+            var doubleData = testData.Select(x => 1.0 * x).ToArray();
+            FITSStream.WriteImage(new Image(doubleData, width, height),
+                FITSImageType.Double, GetPath(file));
+            AssumeExistsAndScheduleForCleanup(file);
+            GenerateAssert<double>(file, doubleData);
+
+
+            file = $"test_sng_{width:0000}x{height:0000}.fits";
+            var singleData = testData.Select(x => 1.0f * x).ToArray();
+            FITSStream.WriteImage(new Image(singleData, width, height),
+                FITSImageType.Single, GetPath(file));
+            AssumeExistsAndScheduleForCleanup(file);
+            GenerateAssert<float>(file, singleData);
+
+
+            file = $"test_ui8_{width:0000}x{height:0000}.fits";
+            var byteData = testData.Select(x => (byte)x).ToArray();
+            FITSStream.WriteImage(new Image(byteData, width, height),
+                FITSImageType.UInt8, GetPath(file));
+            AssumeExistsAndScheduleForCleanup(file);
+            GenerateAssert<byte>(file, byteData);
+
+
+            file = $"test_i16_{width:0000}x{height:0000}.fits";
+            var shortData = testData.Select(x => (short) x).ToArray();
+            FITSStream.WriteImage(new Image(shortData, width, height),
+                FITSImageType.Int16, GetPath(file));
+            AssumeExistsAndScheduleForCleanup(file);
+            GenerateAssert<short>(file, shortData);
+
+
+            file = $"test_i32_{width:0000}x{height:0000}.fits";
+            var intData = testData.ToArray();
+            FITSStream.WriteImage(new Image(intData, width, height),
+                FITSImageType.Int32, GetPath(file));
+            AssumeExistsAndScheduleForCleanup(file);
+            GenerateAssert<int>(file, intData);
+
 
         }
 
-        [Test]
-        public void Test()
+        private void AssumeExistsAndScheduleForCleanup(string path)
         {
-            FITSStream.WriteImage(new Image(_testData.ToArray(), 1024, 512),
-                FITSImageType.Double, "test_dbl.fits");
-            FITSStream.WriteImage(new Image(_testData.Select((x) => (float) x).ToArray(), 1024, 512),
-                FITSImageType.Single, "test_sng.fits");
-            FITSStream.WriteImage(new Image(_testData.Select((x) => (int) x).ToArray(), 1024, 512),
-                FITSImageType.Int32, "test_i32.fits");
-            FITSStream.WriteImage(new Image(_testData.Select((x) => (short) x).ToArray(), 1024, 512),
-                FITSImageType.Int16, "test_i16.fits");
-            FITSStream.WriteImage(new Image(_testData.Select((x) => (byte)x).ToArray(), 1024, 512),
-                FITSImageType.UInt8, "test_ui8.fits");
+            AssumeExists(path);
+            _cleanupActions.Add(() => CleanupFile(path));
         }
+
+        private static string GetPath(in string path)
+            => Path.GetFullPath(Path.IsPathRooted(path)
+                ? path
+                : Path.Combine(TestContext.CurrentContext.TestDirectory, path));
+
+        private static void AssumeExists(in string path)
+            => Assume.That(GetPath(path), Does.Exist,
+                $"File \"{Path.GetFileName(path)}\" was not created.");
+
+        private static void CleanupFile(in string path)
+        {
+            if (File.Exists(GetPath(path)))
+                File.Delete(GetPath(path));
+        }
+            
+
     }
 }
