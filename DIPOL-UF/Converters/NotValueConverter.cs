41b8b6f1f295db7b67d266e316a7d0d822f2f8e1
Ilia Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2019-02-05 23:44:45 +0200

    [Not] converter and property forwarding from camera

---
+++ DIPOL-UF/Converters/NotValueConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace DIPOL_UF.Converters
+{
+    internal class NotValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool b && targetType == typeof(bool))
+                return !b;
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool b && targetType == typeof(bool))
+                return !b;
+            return null;
+        }
+    }
+}
