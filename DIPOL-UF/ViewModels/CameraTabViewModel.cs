4e967dfc79c35e0de31a3817a6d33211f8da263e
Ilia Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2019-02-06 09:42:57 +0200

    Temperature progress bar

--- DIPOL-UF/ViewModels/CameraTabViewModel.cs
+++ DIPOL-UF/ViewModels/CameraTabViewModel.cs
@@ -17,12 +17,14 @@ namespace DIPOL_UF.ViewModels
         public float MinimumAllowedTemperature => 0; // Model.TemperatureRange.Minimum;
         public float MaximumAllowedTemperature => 100; //Model.TemperatureRange.Maximum;
         public bool CanControlTemperature => Model.CanControlTemperature;
+        public bool CanQueryTemperature => Model.CanQueryTemperature;
         public string TabHeader => Model.Alias;
 
+        
         [Reactive]
         public float TargetTemperature { get; set; }
-
         public bool IsAcquiring { [ObservableAsProperty]get; }
+        public float CurrentTemperature { [ObservableAsProperty] get; }
 
         public CameraTabViewModel(CameraTab model) : base(model)
         {
@@ -36,6 +38,12 @@ namespace DIPOL_UF.ViewModels
                  .Select(x => x.IsAcquiring)
                  .ObserveOnUi()
                  .ToPropertyEx(this, x => x.IsAcquiring)
+                 .DisposeWith(_subscriptions);
+
+            Model.WhenTemperatureChecked
+                 .Select(x => x.Temperature)
+                 .ObserveOnUi()
+                 .ToPropertyEx(this, x => x.CurrentTemperature)
                  .DisposeWith(_subscriptions);
         }
 
