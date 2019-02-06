5c0c56cd445d40ac70badff3fd357509a47b7142
Ilia Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2019-02-06 09:42:57 +0200

    Temperature progress bar

--- DIPOL-UF/Models/CameraTab.cs
+++ DIPOL-UF/Models/CameraTab.cs
@@ -1,11 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ANDOR_CS.Classes;
 using ANDOR_CS.Enums;
+using ANDOR_CS.Events;
 using DIPOL_UF.Converters;
+using DynamicData.Binding;
 
 namespace DIPOL_UF.Models
 {
@@ -14,7 +17,10 @@ namespace DIPOL_UF.Models
         public CameraBase Camera { get; }
         public (float Minimum, float Maximum) TemperatureRange { get; }
         public bool CanControlTemperature { get; }
+        public bool CanQueryTemperature { get; }
         public string Alias { get; }
+
+        public IObservable<TemperatureStatusEventArgs> WhenTemperatureChecked { get; private set; }
 
         public CameraTab(CameraBase camera)
         {
@@ -23,8 +29,21 @@ namespace DIPOL_UF.Models
             TemperatureRange = camera.Capabilities.GetFunctions.HasFlag(GetFunction.TemperatureRange)
                 ? camera.Properties.AllowedTemperatures
                 : default;
-            CanControlTemperature = camera.Capabilities.GetFunctions.HasFlag(GetFunction.Temperature);
+            CanControlTemperature = camera.Capabilities.SetFunctions.HasFlag(SetFunction.Temperature);
+            CanQueryTemperature = camera.Capabilities.GetFunctions.HasFlag(GetFunction.Temperature);
             Alias = ConverterImplementations.CameraToStringAliasConversion(camera);
+
+            HookObservables();
+        }
+
+        private void HookObservables()
+        {
+            WhenTemperatureChecked =
+                Observable.FromEventPattern<TemperatureStatusEventHandler, TemperatureStatusEventArgs>(
+                              x => Camera.TemperatureStatusChecked += x,
+                              x => Camera.TemperatureStatusChecked -= x)
+                          .Select(x => x.EventArgs)
+                          .DistinctUntilChanged();
         }
 
         public override void Dispose(bool disposing)
