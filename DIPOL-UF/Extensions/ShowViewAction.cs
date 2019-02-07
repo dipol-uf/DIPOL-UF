e77472cd36234dc8e7bd30b3ddc8c2a983bd8cca
Ilia Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2019-02-07 10:09:38 +0200

    Proof of concept with custom action
    It is possible to completely separate View, VM and Model when creating a new window, propagating an event from Model to View

---
+++ DIPOL-UF/Extensions/ShowViewAction.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Xaml.Behaviors;
+
+namespace DIPOL_UF.Extensions
+{
+    internal class ShowViewAction : TargetedTriggerAction<object>
+    {
+        private static readonly DependencyProperty BindingProperty =
+            DependencyProperty.Register(nameof(Binding), typeof(object), typeof(ShowViewAction));
+
+        private static readonly  DependencyProperty TypeProperty =
+            DependencyProperty.Register(nameof(Type), typeof(object), typeof(ShowViewAction));
+
+        public object Binding
+        {
+            get => GetValue(BindingProperty);
+            set => SetValue(BindingProperty, value);
+        }
+
+        public object Type
+        {
+            get => GetValue(TypeProperty);
+            set => SetValue(TypeProperty, value);
+        }
+
+
+        protected override void Invoke(object parameter)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
