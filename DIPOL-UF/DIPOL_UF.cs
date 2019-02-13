1566a9898d985c6395a1cac1840572ce2ab05cc8
Ilia <ilia.kosenkov.at.gm@gmail.com>
2019-02-13 15:30:51 +0200

    Handling timing issues

--- DIPOL-UF/DIPOL_UF.cs
+++ DIPOL-UF/DIPOL_UF.cs
@@ -103,9 +103,9 @@ namespace DIPOL_UF
                 {
                     setts.SetExposureTime(0.07f);
                     setts.SetImageArea(new Rectangle(1, 1, 512, 512));
-                    setts.SetAcquisitionMode(AcquisitionMode.Kinetic);
-                    setts.SetKineticCycle(10, 0.2f);
-                    setts.SetAccumulationCycle(3, 0.2f);
+                    setts.SetAcquisitionMode(AcquisitionMode.SingleScan);
+                    //setts.SetKineticCycle(10, 0.2f);
+                    //setts.SetAccumulationCycle(10, 0.2f);
                     setts.SetReadoutMode(ReadMode.FullImage);
                     setts.SetTriggerMode(TriggerMode.Internal);
                     setts.ApplySettings(out _);
