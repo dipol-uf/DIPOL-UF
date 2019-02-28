81b35f9222e150573870ffb4aa7c447c7a16e3c9
Ilia Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2019-02-28 21:51:47 +0200

    Refactoring

--- DIPOL-UF/Converters/ValueTupleToValueValueConverter.cs
+++ DIPOL-UF/Converters/ValueTupleToValueValueConverter.cs
@@ -1,8 +1,31 @@
-﻿using System;
+﻿//    This file is part of Dipol-3 Camera Manager.
+
+//     MIT License
+//     
+//     Copyright(c) 2018-2019 Ilia Kosenkov
+//     
+//     Permission is hereby granted, free of charge, to any person obtaining a copy
+//     of this software and associated documentation files (the "Software"), to deal
+//     in the Software without restriction, including without limitation the rights
+//     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//     copies of the Software, and to permit persons to whom the Software is
+//     furnished to do so, subject to the following conditions:
+//     
+//     The above copyright notice and this permission notice shall be included in all
+//     copies or substantial portions of the Software.
+//     
+//     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//     FITNESS FOR A PARTICULAR PURPOSE AND NONINFINGEMENT. IN NO EVENT SHALL THE
+//     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//     SOFTWARE.
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Windows.Data;
-using System.Reflection;
 using System.Globalization;
 
 
@@ -12,8 +35,7 @@ namespace DIPOL_UF.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            if (parameter is string fieldName && 
-                value ?.GetType() is Type type)
+            if (parameter is string fieldName)
             {
                 if (value is IEnumerable collection)
                 {
@@ -35,7 +57,8 @@ namespace DIPOL_UF.Converters
                     return array;
 
                 }
-                else return ConverterImplementations.FieldAccessConversion(value, fieldName);
+
+                return ConverterImplementations.FieldAccessConversion(value, fieldName);
 
 
             }
