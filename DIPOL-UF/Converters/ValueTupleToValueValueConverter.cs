f9973d27dcff8f38a8d20ba13a12738d14f142a7
Ilia Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2019-02-28 21:45:02 +0200

    Several improvements

--- DIPOL-UF/Converters/ValueTupleToValueValueConverter.cs
+++ DIPOL-UF/Converters/ValueTupleToValueValueConverter.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Windows.Data;
 using System.Reflection;
 using System.Globalization;
@@ -10,38 +12,39 @@ namespace DIPOL_UF.Converters
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            Type type;
-            if ((type = value?.GetType())!= null && parameter is string fieldName)
+            if (parameter is string fieldName && 
+                value ?.GetType() is Type type)
             {
-                if (type.BaseType == typeof(Array))
+                if (value is IEnumerable collection)
                 {
-                    var array = value as Array;
-                    Type innerType;
-                    FieldInfo fi;
-                    if (array.Length > 0 &&
-                        (innerType = array.GetValue(0).GetType()).IsValueType &&
-                        (fi = innerType.GetField(fieldName, BindingFlags.Public | BindingFlags.Instance)) != null)
+                    var enumer = collection.GetEnumerator();
+                    var result = new List<object>();
+                    while (enumer.MoveNext())
                     {
-                        Array result = Array.CreateInstance(fi.FieldType, array.Length);
-                        for (int i = 0; i < array.Length; i++)
-                            result.SetValue(fi.GetValue(array.GetValue(i)), i);
-
-                        return result;
+                        var item = ConverterImplementations.FieldAccessConversion(enumer.Current, fieldName);
+                        result.Add(item);
                     }
-                }
-                else if (type.IsValueType)
-                    return type
-                        .GetField(fieldName, BindingFlags.Public | BindingFlags.Instance)
-                        ?.GetValue(value);
 
-                
+                    var array = Array.CreateInstance(
+                        result.Count > 0 ? result[0].GetType() : typeof(object),
+                        result.Count);
+
+                    for (var i = 0; i < array.Length; i++)
+                        array.SetValue(result[i], i);
+
+                    return array;
+
+                }
+                else return ConverterImplementations.FieldAccessConversion(value, fieldName);
+
+
             }
             return null;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotSupportedException("Backward convertion is not supported");
-        }
+            => throw new NotSupportedException(string.Format(
+                Properties.Localization.General_OperationNotSupported,
+                $"{nameof(ValueTupleToValueValueConverter)}.{nameof(ConvertBack)}"));
     }
 }
