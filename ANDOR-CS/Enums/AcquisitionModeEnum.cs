9fc378223071dabaaac762870848aca16ed1dd92
Ilia Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2019-03-01 23:01:14 +0200

    Fixing settings availability

--- ANDOR-CS/Enums/AcquisitionModeEnum.cs
+++ ANDOR-CS/Enums/AcquisitionModeEnum.cs
@@ -2,7 +2,7 @@
 
 //     MIT License
 //     
-//     Copyright(c) 2018 Ilia Kosenkov
+//     Copyright(c) 2018-2019 Ilia Kosenkov
 //     
 //     Permission is hereby granted, free of charge, to any person obtaining a copy
 //     of this software and associated documentation files (the "Software"), to deal
@@ -42,6 +42,9 @@ namespace ANDOR_CS.Enums
     [DataContract]
     public enum AcquisitionMode : uint
     {
+        [Description("Unknown")]
+        [EnumMember]
+        Unknown = 0,
 
         /// <summary>
         /// Single Scan
