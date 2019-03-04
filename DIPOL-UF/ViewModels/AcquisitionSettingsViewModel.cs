d54a3394e6d90093cd57dea02b70ccee908c8d9d
Ilia Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2019-03-04 13:04:49 +0200

    Refactored current state of acquisition settings

--- DIPOL-UF/ViewModels/AcquisitionSettingsViewModel.cs
+++ DIPOL-UF/ViewModels/AcquisitionSettingsViewModel.cs
@@ -24,35 +24,27 @@
 
 using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Globalization;
 using System.Linq;
 using System.Windows;
-using System.Text.RegularExpressions;
-using System.Reflection;
 using System.Threading.Tasks;
 using System.IO;
 using System.Linq.Expressions;
 using System.Reactive.Disposables;
 using System.Reactive.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
-using System.Windows.Input;
 using ANDOR_CS.Classes;
 using ANDOR_CS.DataStructures;
 using ANDOR_CS.Enums;
 using DIPOL_UF.Commands;
-using DIPOL_UF.Properties;
 using DynamicData;
 using DynamicData.Binding;
-using DynamicData.Kernel;
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 
 using static DIPOL_UF.Validators.Validate;
 using EnumConverter = ANDOR_CS.Classes.EnumConverter;
-using Size = ANDOR_CS.DataStructures.Size;
 
 // ReSharper disable UnassignedGetOnlyAutoProperty
 
@@ -115,7 +107,7 @@ namespace DIPOL_UF.ViewModels
        /// <summary>
         /// Collection of supported by a given Camera settings.
         /// </summary>
-        public HashSet<string> SupportedSettings { get; private set; }
+        public HashSet<string> SupportedSettings { get; }
 
         /// <summary>
         /// Collection of settings that can be set now.
@@ -123,7 +115,6 @@ namespace DIPOL_UF.ViewModels
         public HashSet<string> AllowedSettings
         {
             get;
-            private set;
         }
 
         /// <summary>
@@ -149,418 +140,6 @@ namespace DIPOL_UF.ViewModels
 
         public IObservableCollection<ReadMode> AvailableReadModes { get; }
         = new ObservableCollectionExtended<ReadMode>();
-
-
-        ///// <summary>
-        ///// Index of VS Speed.
-        ///// </summary>
-        //public int VsSpeed
-        //{
-        //    get => model.VSSpeed?.Index ?? -1;
-        //    set
-        //    {
-        //        try
-        //        {
-        //            model.SetVSSpeed(value < 0 ? 0 : value);
-        //            ValidateProperty(null);
-        //            //RaisePropertyChanged();
-        //        }
-        //        catch (Exception e)
-        //        {
-        //            ValidateProperty(e);
-        //        }
-
-        //    }
-        //}
-        ///// <summary>
-        ///// VS Amplitude.
-        ///// </summary>
-        //public VSAmplitude? VSAmplitudeValue
-        //{
-        //    get => model.VSAmplitude;
-        //    set
-        //    {
-        //        try
-        //        {
-        //            model.SetVSAmplitude(value ?? VSAmplitude.Normal);
-        //            ValidateProperty(null);
-        //            //RaisePropertyChanged();
-        //        }
-        //        catch (Exception e)
-        //        {
-        //            ValidateProperty(e);
-        //        }
-        //    }
-        //}
-        ///// <summary>
-        ///// Analog-Digital COnverter index.
-        ///// </summary>
-        //public int ADConverterIndex
-        //{
-        //    get => model.ADConverter?.Index ?? -1;
-        //    set
-        //    {
-        //        try
-        //        {
-        //            model.SetADConverter(value < 0 ? 0 : value);
-        //            ValidateProperty(null);
-        //            //RaisePropertyChanged();
-        //        }
-        //        catch (Exception e)
-        //        {
-        //            ValidateProperty(e);
-        //        }
-        //    }
-        //}
-        ///// <summary>
-        ///// Output OutputAmplifier index.
-        ///// </summary>
-        //public int OutputAmplifierIndex
-        //{
-        //    get => model.OutputAmplifier?.Index ?? -1;
-        //    set
-        //    {
-        //        try
-        //        {
-        //            model.SetOutputAmplifier(Camera.Properties.OutputAmplifiers[value < 0 ? 0 : value].OutputAmplifier);
-        //            ValidateProperty(null);
-        //            //RaisePropertyChanged();
-        //        }
-        //        catch (Exception e)
-        //        {
-        //            ValidateProperty(e);
-        //        }
-        //    }
-        //}
-        ///// <summary>
-        ///// HS Speed.
-        ///// </summary>
-        //public int HSSpeedIndex
-        //{
-        //    get => model.HSSpeed?.Index ?? -1;
-        //    set
-        //    {
-
-        //        try
-        //        {
-        //            model.SetHSSpeed(value < 0 ? 0 : value);
-        //            ValidateProperty(null);
-        //            //RaisePropertyChanged();
-        //        }
-        //        catch (Exception e)
-        //        {
-        //            ValidateProperty(e);
-        //        }
-
-        //    }
-        //}
-        ///// <summary>
-        ///// Index of Pre OutputAmplifier Gain.
-        ///// </summary>
-        //public int PreAmpGainIndex
-        //{
-        //    get => model.PreAmpGain?.Index ?? -1;
-        //    set
-        //    {
-        //        try
-        //        {
-        //            model.SetPreAmpGain(value < 0 ? 0 : value);
-        //            ValidateProperty(null);
-        //            //RaisePropertyChanged();
-
-        //        }
-        //        catch (Exception e)
-        //        {
-        //            ValidateProperty(e);
-        //        }
-        //    }
-        //}
-        ///// <summary>
-        ///// Acquisition mode value.
-        ///// </summary>
-        //public AcquisitionMode? AcquisitionModeValue
-        //{
-        //    get
-        //    {
-        //        if (model.AcquisitionMode?.HasFlag(AcquisitionMode.FrameTransfer) ?? false)
-        //            return model.AcquisitionMode ^ AcquisitionMode.FrameTransfer;
-        //        else
-        //            return model.AcquisitionMode;
-        //    }
-        //    set
-        //    {
-        //        try
-        //        {
-        //            model.SetAcquisitionMode(value ?? AcquisitionMode.SingleScan);
-        //            ValidateProperty(null);
-        //            //RaisePropertyChanged();
-        //        }
-        //        catch (Exception e)
-        //        {
-        //            ValidateProperty(e);
-        //        }
-        //    }
-        //}
-        ///// <summary>
-        ///// Frame transfer flag; applied to acquisition mode
-        ///// </summary>
-        //public bool FrameTransferValue
-        //{
-        //    get => model.AcquisitionMode?.HasFlag(AcquisitionMode.FrameTransfer) ?? false;
-        //    set
-        //    {
-        //        try
-        //        {
-        //            if (value)
-        //            {
-        //                if (!model.AcquisitionMode?.HasFlag(AcquisitionMode.FrameTransfer) ?? false)
-        //                {
-        //                    model.SetAcquisitionMode((model.AcquisitionMode | AcquisitionMode.FrameTransfer) ?? AcquisitionMode.FrameTransfer);
-        //                    ValidateProperty(null);
-        //                    //RaisePropertyChanged();
-        //                }
-        //            }
-        //            else
-        //            {
-        //                if (model.AcquisitionMode?.HasFlag(AcquisitionMode.FrameTransfer) ?? false)
-        //                {
-        //                    model.SetAcquisitionMode((model.AcquisitionMode ^ AcquisitionMode.FrameTransfer) ?? AcquisitionMode.SingleScan);
-        //                    ValidateProperty(null);
-        //                    //RaisePropertyChanged();
-        //                }
-        //            }
-        //        }
-        //        catch (Exception e)
-        //        {
-        //            ValidateProperty(e);
-        //        }
-
-        //    }
-        //}
-        ///// <summary>
-        ///// Read mode value
-        ///// </summary>
-        //public ReadMode? ReadoutModeValue
-        //{
-        //    get => model.ReadoutMode;
-        //    set
-        //    {
-        //        try
-        //        {
-        //            model.SetReadoutMode(value ?? ReadMode.FullImage);
-        //            ValidateProperty(null);
-        //            //RaisePropertyChanged();
-        //        }
-        //        catch (Exception e)
-        //        {
-        //            ValidateProperty(e);
-        //        }
-        //    }
-        //}
-        ///// <summary>
-        ///// Trigger mode value
-        ///// </summary>
-        //public TriggerMode? TriggerModeValue
-        //{
-        //    get => model.TriggerMode;
-        //    set
-        //    {
-        //        try
-        //        {
-        //            model.SetTriggerMode(value ?? TriggerMode.Internal);
-        //            ValidateProperty(null);
-        //            //RaisePropertyChanged();
-        //        }
-        //        catch (Exception e)
-        //        {
-        //            ValidateProperty(e);
-        //        }
-        //    }
-        //}
-        ///// <summary>
-        ///// Exposure time; text field
-        ///// </summary>
-        //public string ExposureTimeValueText
-        //{
-        //    get => model.ExposureTime?.ToString();
-        //    set
-        //    {
-        //        try
-        //        {
-        //            if (string.IsNullOrWhiteSpace(value))
-        //            {
-        //                model.SetExposureTime(0f);
-        //                ValidateProperty(null);
-        //                //RaisePropertyChanged();
-        //            }
-        //            else if (float.TryParse(value, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out float flVal))
-        //            {
-        //                model.SetExposureTime(flVal);
-        //                ValidateProperty(null);
-        //                //RaisePropertyChanged();
-        //            }
-        //            else
-        //                ValidateProperty(new ArgumentException("Provided value is not a number."));
-
-        //        }
-        //        catch (Exception e)
-        //        {
-        //            ValidateProperty(e);
-        //        }
-        //    }
-
-        //}
-        ///// <summary>
-        ///// EM CCD gain; text field
-        ///// </summary>
-        //public string EMCCDGainValueText
-        //{
-        //    get => model.EMCCDGain?.ToString();
-        //    set
-        //    {
-        //        try
-        //        {
-        //            if (string.IsNullOrWhiteSpace(value))
-        //            {
-        //                model.SetEMCCDGain(Camera.Properties.EMCCDGainRange.Low);
-        //                ValidateProperty();
-        //                //RaisePropertyChanged();
-        //            }
-        //            else if (int.TryParse(value, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out int intVal))
-        //            {
-        //                model.SetEMCCDGain(intVal);
-        //                ValidateProperty();
-        //                //RaisePropertyChanged();
-        //            }
-        //            else
-        //                ValidateProperty(new ArgumentException("Provided value is not a number."));
-        //        }
-        //        catch (Exception e)
-        //        {
-        //            ValidateProperty(e);
-        //        }
-        //        finally
-        //        {
-        //            //RaisePropertyChanged();
-        //        }
-        //    }
-        //}
-
-        //public string ImageArea_X1
-        //{
-        //    get => model.ImageArea?.X1.ToString();
-        //    set {
-        //        try
-        //        {
-        //            if (int.TryParse(value,
-        //                System.Globalization.NumberStyles.Any,
-        //                System.Globalization.NumberFormatInfo.InvariantInfo, out var intVal))
-        //            {
-        //                var rect = new Rectangle(
-        //                    intVal,
-        //                    model.ImageArea?.Y1 ?? 1,
-        //                    model.ImageArea?.X2 > intVal ? model.ImageArea.Value.X2 : intVal + 1,
-        //                    model.ImageArea?.Y2 ?? 2);
-        //                model.SetImageArea(rect);
-        //                ValidateProperty(null);
-        //            }
-        //            else
-        //                ValidateProperty(new ArgumentException("Provided value is not a number"));
-        //        }
-        //        catch (Exception e)
-        //        {
-        //            ValidateProperty(e);
-        //        }
-
-        //    }
-        //}
-        //public string ImageArea_X2
-        //{
-        //    get => model.ImageArea?.X2.ToString();
-        //    set
-        //    {
-        //        try
-        //        {
-        //            if (int.TryParse(value,
-        //                System.Globalization.NumberStyles.Any,
-        //                System.Globalization.NumberFormatInfo.InvariantInfo, out var intVal))
-        //            {
-        //                var rect = new Rectangle(
-        //                    model.ImageArea?.X1 < intVal ? model.ImageArea.Value.X1 : intVal - 1,
-        //                    model.ImageArea?.Y1 ?? 1,
-        //                    intVal,
-        //                    model.ImageArea?.Y2 ?? 2);
-        //                model.SetImageArea(rect);
-        //                ValidateProperty(null);
-        //            }
-        //            else
-        //                ValidateProperty(new ArgumentException("Provided value is not a number"));
-        //        }
-        //        catch (Exception e)
-        //        {
-        //            ValidateProperty(e);
-        //        }
-
-        //    }
-        //}
-        //public string ImageArea_Y1
-        //{
-        //    get => model.ImageArea?.Y1.ToString();
-        //    set
-        //    {
-        //        try
-        //        {
-        //            if (int.TryParse(value,
-        //                System.Globalization.NumberStyles.Any,
-        //                System.Globalization.NumberFormatInfo.InvariantInfo, out var intVal))
-        //            {
-        //                var rect = new Rectangle(
-        //                    model.ImageArea?.X1 ?? 1,
-        //                    intVal,
-        //                    model.ImageArea?.X2 ?? 2,
-        //                    model.ImageArea?.Y2 > intVal ? model.ImageArea.Value.Y2 : intVal + 1);
-        //                model.SetImageArea(rect);
-        //                ValidateProperty(null);
-        //            }
-        //            else
-        //                ValidateProperty(new ArgumentException("Provided value is not a number"));
-        //        }
-        //        catch (Exception e)
-        //        {
-        //            ValidateProperty(e);
-        //        }
-
-        //    }
-        //}
-        //public string ImageArea_Y2
-        //{
-        //    get => model.ImageArea?.Y2.ToString();
-        //    set
-        //    {
-        //        try
-        //        {
-        //            if (int.TryParse(value,
-        //                System.Globalization.NumberStyles.Any,
-        //                System.Globalization.NumberFormatInfo.InvariantInfo, out var intVal))
-        //            {
-        //                var rect = new Rectangle(
-        //                    model.ImageArea?.X1 ?? 1,
-        //                    model.ImageArea?.Y1 < intVal ? model.ImageArea.Value.Y1 : intVal - 1,
-        //                    model.ImageArea?.X2 ?? 2,
-        //                    intVal);
-        //                model.SetImageArea(rect);
-        //                ValidateProperty(null);
-        //            }
-        //            else
-        //                ValidateProperty(new ArgumentException("Provided value is not a number"));
-        //        }
-        //        catch (Exception e)
-        //        {
-        //            ValidateProperty(e);
-        //        }
-
-        //    }
-        //}
 
         public AcquisitionSettingsViewModel(ReactiveWrapper<SettingsBase> model)
             : base(model)
@@ -1020,21 +599,6 @@ namespace DIPOL_UF.ViewModels
 
         }
 
-        private void InitializeAllowedSettings()
-        {
-            //new KeyValuePair<string, bool>(nameof(model.HSSpeed),
-            //    ADConverterIndex >= 0
-            //    && OutputAmplifierIndex >= 0),
-            //new KeyValuePair<string, bool>(nameof(model.PreAmpGain),
-            //    ADConverterIndex >= 0
-            //    && OutputAmplifierIndex >= 0
-            //    && HSSpeedIndex >= 0),
-            //new KeyValuePair<string, bool>(nameof(model.EMCCDGain),
-            //    (OutputAmplifierIndex >= 0)
-            //    && Camera.Properties.OutputAmplifiers[OutputAmplifierIndex].OutputAmplifier == OutputAmplification.Conventional),
-
-        }
-
         private void InitializeCommands()
         {
             SubmitCommand = new DelegateCommand(
@@ -1305,7 +869,7 @@ namespace DIPOL_UF.ViewModels
         #region V2
 
         [Reactive]
-        public string ExposureTimeText { get; set; } = null;
+        public string ExposureTimeText { get; set; }
 
         // -1 is the default selected index in the list, equivalent to
         // [SelectedItem] = null in case of nullable properties
@@ -1337,7 +901,7 @@ namespace DIPOL_UF.ViewModels
         public TriggerMode? TriggerMode { get; set; }
 
         [Reactive]
-        public string EmCcdGainText { get; set; } = null;
+        public string EmCcdGainText { get; set; }
 
         [Reactive]
         // ReSharper disable once InconsistentNaming
