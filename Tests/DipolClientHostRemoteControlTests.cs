f1b12edf125a5090bbbe8e72902360eeff8347cc
Ilia Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2019-03-27 09:49:35 +0200

    Working on [RemoteControl]

--- Tests/DipolClientHostRemoteControlTests.cs
+++ Tests/DipolClientHostRemoteControlTests.cs
@@ -2,7 +2,7 @@
 
 //     MIT License
 //     
-//     Copyright(c) 2018 Ilia Kosenkov
+//     Copyright(c) 2018-2019 Ilia Kosenkov
 //     
 //     Permission is hereby granted, free of charge, to any person obtaining a copy
 //     of this software and associated documentation files (the "Software"), to deal
@@ -22,7 +22,7 @@
 //     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 //     SOFTWARE.
 
-//#define HOST_IN_PROCESS
+#define HOST_IN_PROCESS
 
 using System;
 using System.Diagnostics.CodeAnalysis;
