1907877d88f5e9729c7ef42de85f049433fc9478
Ilia Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2019-03-28 13:27:24 +0200

    Improved asyncresult

--- DIPOL-Remote/Classes/AsyncResult.cs
+++ DIPOL-Remote/Classes/AsyncResult.cs
@@ -1,32 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Win32.SafeHandles;
 
 namespace DIPOL_Remote.Classes
 {
-    public class AsyncCameraResult : IAsyncResult, IDisposable
+
+    internal class AsyncVoidResult : IAsyncResult, IDisposable
     {
         private readonly ManualResetEventSlim _event;
         public bool IsCompleted => Task?.IsCompleted ?? false;
-        public WaitHandle AsyncWaitHandle => throw new NotSupportedException();
+        public WaitHandle AsyncWaitHandle => _event.WaitHandle;
         public object AsyncState { get; }
         public bool CompletedSynchronously => false;
         public Task Task { get; }
+        public AsyncCallback Callback { get; }
 
-        public AsyncCameraResult(Task cameraCreationTask, object state)
+        public AsyncVoidResult(Task task, AsyncCallback callback, object state)
         {
+            Task = task ?? throw new ArgumentNullException(nameof(task));
+            Callback = callback ?? throw new ArgumentNullException(nameof(callback));
+
             _event = new ManualResetEventSlim(false);
             AsyncState = state;
 
-            Task = cameraCreationTask;
-            if(cameraCreationTask?.IsCompleted == true)
-                _event.Set();
-            else
-                Task.ConfigureAwait(false).GetAwaiter().OnCompleted(() => _event.Set());
+            Task.GetAwaiter().OnCompleted(FinalizeInvocation);
+        }
+
+        private void FinalizeInvocation()
+        {
+            _event.Set();
+            Callback.Invoke(this);
         }
 
         public void Dispose()
