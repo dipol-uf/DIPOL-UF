e34f51d6dff3b22e49c93925632e7b8b5978f054
Ilia Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2019-03-29 13:45:19 +0200

    Updated faults

--- DIPOL-Remote/Faults/ServiceFault.cs
+++ DIPOL-Remote/Faults/ServiceFault.cs
@@ -29,26 +29,13 @@ using System.ServiceModel;
 namespace DIPOL_Remote.Faults
 {
     [DataContract(IsReference = false)]
-    public class ServiceException
+    public class ServiceFault
     {
         public static readonly FaultReason CameraCommunicationReason
             = new FaultReason($"Error occured while communicating with {nameof(ANDOR_CS.Classes.Camera)} object.");
 
         public static readonly FaultReason GeneralServiceErrorReason
             = new FaultReason("General error occured while communicating with service.");
-
-        public static readonly FaultReason IllegalSessionReason
-            = new FaultReason("Accessed camera belongs to another remote session.");
-
-        public static FaultException<ServiceException> IllegalSessionFaultException()
-            => new FaultException<ServiceException>(
-                        new ServiceException()
-                        {
-                            Message = "Cannot remove camera used in our session.",
-                            Details = "Specified camera is used in another session and therefore cannot be controlled from current session.",
-                            MethodName = ""
-                        },
-                        IllegalSessionReason);
 
         [DataMember(IsRequired = true, Order = 0)]
         public string Message;
