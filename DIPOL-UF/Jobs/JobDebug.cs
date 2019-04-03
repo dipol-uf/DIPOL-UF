cf4200b243ca7725ed8865085384cb6b9eae62f1
Ilia Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2019-04-03 12:38:15 +0300

    Implementing jobs to handle observation cycle

---
+++ DIPOL-UF/Jobs/JobDebug.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel.Configuration;
+using System.Threading.Tasks;
+using DIPOL_UF.Jobs;
+
+namespace DIPOL_UF
+{
+    // WATCH : partial class that hides real [Main]
+    public static partial class DipolUfApp
+    {
+        public static async Task Main()
+        {
+            var job = new Jobs.Job
+            {
+                _actions = new List<JobAction>
+                {
+                    new MotorAction(),
+                    new RepeatAction(
+                        new List<JobAction>
+                        {
+                            new CameraAction(@"camera/expose 1,3"),
+                            new MotorAction(@"motor/rotate -3.5")
+                        }, 4),
+                    new MotorAction()
+                }
+            };
+
+            await job.Run();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ DIPOL-UF/Jobs/RepeatAction.cs
@@ -0,0 +1,56 @@
+﻿//    This file is part of Dipol-3 Camera Manager.
+//
+//     MIT License
+//     
+//     Copyright(c) 2018-2019 Ilia Kosenkov
+//     
+//     Permission is hereby granted, free of charge, to any person obtaining a copy
+//     of this software and associated documentation files (the "Software"), to deal
+//     in the Software without restriction, including without limitation the rights
+//     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//     copies of the Software, and to permit persons to whom the Software is
+//     furnished to do so, subject to the following conditions:
+//     
+//     The above copyright notice and this permission notice shall be included in all
+//     copies or substantial portions of the Software.
+//     
+//     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//     FITNESS FOR A PARTICULAR PURPOSE AND NONINFINGEMENT. IN NO EVENT SHALL THE
+//     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//     SOFTWARE.
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace DIPOL_UF.Jobs
+{
+    class RepeatAction : JobAction
+    {
+        public int Number { get; private set; }
+
+        private readonly List<JobAction> _actions;
+
+        public RepeatAction(List<JobAction> actions, int number)
+        {
+            // WATCH : Prototyping constructor
+            _actions = actions;
+            Number = number;
+        }
+
+        public override async Task Execute()
+        {
+            for (var i = 0; i < Number; i++)
+            {
+                Console.WriteLine($@"{DateTime.Now:HH:mm:ss.fff} Repeat block {i:00} starts");
+                foreach (var action in _actions)
+                    await action.Execute();
+                Console.WriteLine($"{DateTime.Now:HH:mm:ss.fff} Repeat block {i:00} ends\r\n");
+            }
+
+        }
+    }
+}
