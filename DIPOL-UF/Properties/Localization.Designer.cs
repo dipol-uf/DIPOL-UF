29df49a234e91ef5486860103bb55333fe17ab26
Ilia Kosenkov <ilia.kosenkov.at.gm@gmail.com>
2019-04-22 16:01:08 +0300

    Fixed step motor angle display

--- DIPOL-UF/Properties/Localization.Designer.cs
+++ DIPOL-UF/Properties/Localization.Designer.cs
@@ -19,7 +19,7 @@ namespace DIPOL_UF.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     public class Localization {
@@ -548,6 +548,15 @@ namespace DIPOL_UF.Properties {
         public static string CameraTab_Shutter_Internal {
             get {
                 return ResourceManager.GetString("CameraTab_Shutter_Internal", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to {0,6}°.
+        /// </summary>
+        public static string CameraTab_StepMotorAngleFormat {
+            get {
+                return ResourceManager.GetString("CameraTab_StepMotorAngleFormat", resourceCulture);
             }
         }
         
