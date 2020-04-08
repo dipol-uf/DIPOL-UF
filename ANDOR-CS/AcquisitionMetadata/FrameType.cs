3d1d26c94d89d515edd706930ebe1dacae4f78bc
Ilia <ilia.kosenkov.at.gm@gmail.com>
2020-04-08 15:23:29 +0300

    Passing more information through `Request`

---
+++ ANDOR-CS/AcquisitionMetadata/FrameType.cs
@@ -0,0 +1,17 @@
+﻿#nullable enable
+#pragma warning disable 1591
+using System.Runtime.Serialization;
+
+namespace ANDOR_CS.AcquisitionMetadata
+{
+    [DataContract]
+    public enum FrameType : byte
+    {
+        [EnumMember]
+        Light,
+        [EnumMember]
+        Dark,
+        [EnumMember]
+        Bias
+    }
+}
