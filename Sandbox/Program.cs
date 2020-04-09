508b4090ef8554dd836f933114a620012617b981
Ilia <ilia.kosenkov.at.gm@gmail.com>
2020-04-09 17:26:12 +0300

    Sandbox project

---
+++ Sandbox/Program.cs
@@ -0,0 +1,33 @@
+﻿#nullable enable
+using System.Linq;
+using System.Threading.Tasks;
+using ANDOR_CS.Classes;
+using Enumerable = System.Linq.Enumerable;
+
+namespace Sandbox
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await JsonSerializationTest();
+        }
+
+        private static async Task JsonSerializationTest()
+        {
+            var cams = await Task.WhenAll(Enumerable.Range(0, 3)
+                .Select(x => DebugCamera.CreateAsync(x)));
+            try
+            {
+            }
+            finally
+            {
+                if (cams is { })
+                    foreach(var cam in cams)
+                        cam?.Dispose();
+            }
+
+
+        }
+    }
+}
