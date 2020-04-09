928f780866312eff2c037247d872f0acb77d054e
Ilia <ilia.kosenkov.at.gm@gmail.com>
2020-04-09 16:39:11 +0300

    new `Target` type

---
+++ DIPOL-UF/Jobs/Target1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using ANDOR_CS.Classes;
+using DIPOL_UF.Enums;
+
+#nullable enable
+// TODO : Remove in prod
+[assembly:InternalsVisibleTo("")]
+
+namespace DIPOL_UF.Jobs
+{
+    internal class Target1
+    {
+        public string? StarName { get; set; }
+        public CycleType CycleType { get; set; }
+
+        public Dictionary<string, Dictionary<string, object?>?>? UniqueParameters { get; set; }
+
+        public Dictionary<string, SettingsBase>? AcquisitionParameters { get; set; }
+        
+    }
+}
