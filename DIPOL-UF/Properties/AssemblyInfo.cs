877d3ed20df96cdae6e99d6f5ea2b06a46338377
Ilia <ilia.kosenkov@outlook.com>
2021-01-19 13:24:20 +0200

    Bumped version

--- DIPOL-UF/Properties/AssemblyInfo.cs
+++ DIPOL-UF/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("DIPOL-UF")]
-[assembly: AssemblyCopyright("Copyright Ilia Kosenkov ©  2017-2020")]
+[assembly: AssemblyCopyright("Copyright Ilia Kosenkov ©  2017-2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.2.*")]
+[assembly: AssemblyVersion("2.3.*")]
 //[assembly: AssemblyFileVersion("1.0.*")]
