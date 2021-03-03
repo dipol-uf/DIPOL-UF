d812fb72f1e9627dac455e643981de41c2403c4f
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2021-03-03 21:53:07 +0300

    Using constant

--- DipolImage/Ops.cs
+++ DipolImage/Ops.cs
@@ -12,17 +12,17 @@ namespace DipolImage
         // x ^ (...) is either x ^ x ^ y or x ^ 0
         // if x < y then y else x
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int Max(int x, int y) => (int) (x ^ ((x ^ y) & (((long) x - y) >> (sizeof(long) * 8 - 1))));
+        public static int Max(int x, int y) => (int) (x ^ ((x ^ y) & (((long) x - y) >> (sizeof(long) * ByteSize - 1))));
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static uint Max(uint x, uint y) => (uint)(x ^ ((x ^ y) & (((long)x - y) >> (sizeof(long) * 8 - 1))));
+        public static uint Max(uint x, uint y) => (uint)(x ^ ((x ^ y) & (((long)x - y) >> (sizeof(long) * ByteSize - 1))));
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static short Max(short x, short y) => (short)(x ^ ((x ^ y) & ((x - y) >> (sizeof(int) * 8 - 1))));
+        public static short Max(short x, short y) => (short)(x ^ ((x ^ y) & ((x - y) >> (sizeof(int) * ByteSize - 1))));
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ushort Max(ushort x, ushort y) => (ushort)(x ^ ((x ^ y) & ((x - y) >> (sizeof(int) * 8 - 1))));
+        public static ushort Max(ushort x, ushort y) => (ushort)(x ^ ((x ^ y) & ((x - y) >> (sizeof(int) * ByteSize - 1))));
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static byte Max(byte x, byte y) => (byte)(x ^ ((x ^ y) & ((x - y) >> (sizeof(int) * 8 - 1))));
+        public static byte Max(byte x, byte y) => (byte)(x ^ ((x ^ y) & ((x - y) >> (sizeof(int) * ByteSize - 1))));
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static float Max(float x, float y) =>
@@ -45,17 +45,17 @@ namespace DipolImage
             };
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static int Min(int x, int y) => (int)(y ^ ((x ^ y) & (((long)x - y) >> (sizeof(long) * 8 - 1))));
+        public static int Min(int x, int y) => (int)(y ^ ((x ^ y) & (((long)x - y) >> (sizeof(long) * ByteSize - 1))));
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static uint Min(uint x, uint y) => (uint)(y ^ ((x ^ y) & (((long)x - y) >> (sizeof(long) * 8 - 1))));
+        public static uint Min(uint x, uint y) => (uint)(y ^ ((x ^ y) & (((long)x - y) >> (sizeof(long) * ByteSize - 1))));
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static short Min(short x, short y) => (short)(y ^ ((x ^ y) & ((x - y) >> (sizeof(int) * 8 - 1))));
+        public static short Min(short x, short y) => (short)(y ^ ((x ^ y) & ((x - y) >> (sizeof(int) * ByteSize - 1))));
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ushort Min(ushort x, ushort y) => (ushort)(y ^ ((x ^ y) & ((x - y) >> (sizeof(int) * 8 - 1))));
+        public static ushort Min(ushort x, ushort y) => (ushort)(y ^ ((x ^ y) & ((x - y) >> (sizeof(int) * ByteSize - 1))));
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static byte Min(byte x, byte y) => (byte)(y ^ ((x ^ y) & ((x - y) >> (sizeof(int) * 8 - 1))));
+        public static byte Min(byte x, byte y) => (byte)(y ^ ((x ^ y) & ((x - y) >> (sizeof(int) * ByteSize - 1))));
 
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
