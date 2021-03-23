b4874701c340b76eda0562ea7a3bbb83656ceefd
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2021-03-23 13:24:16 +0200

    Extracting base class

---
+++ DipolImage/ImageConstructor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DipolImage
+{
+    internal interface IImageConstructor
+    {
+        ImageBase CreateTyped<T>(ReadOnlySpan<T> data, int width, int height) where T : unmanaged;
+        ImageBase CreateAndFill(int width, int height, TypeCode type, ImageInitializers.ImageInitializer initializer);
+        ImageBase CreateAndFill<TState>(
+            int width, int height, TypeCode type, ImageInitializers.ImageInitializer<TState> initializer, TState state
+        );
+    }
+    
+}
---
+++ DipolImage/ImageInitializers.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DipolImage
+{
+    internal class ImageInitializers
+    {
+        internal delegate void ImageInitializer(Span<byte> view);
+        internal delegate void ImageInitializer<in TState>(Span<byte> view, TState state);
+    }
+}
