d49c53a3910637981879568899e4c34cb701919a
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2021-05-26 10:38:08 +0300

    Updating RFS

--- src/DIPOL-UF/Jobs/JobManager.MotorAction.cs
+++ src/DIPOL-UF/Jobs/JobManager.MotorAction.cs
@@ -1,30 +1,5 @@
-﻿//    This file is part of Dipol-3 Camera Manager.
-
-//     MIT License
-//     
-//     Copyright(c) 2018-2019 Ilia Kosenkov
-//     
-//     Permission is hereby granted, free of charge, to any person obtaining a copy
-//     of this software and associated documentation files (the "Software"), to deal
-//     in the Software without restriction, including without limitation the rights
-//     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//     copies of the Software, and to permit persons to whom the Software is
-//     furnished to do so, subject to the following conditions:
-//     
-//     The above copyright notice and this permission notice shall be included in all
-//     copies or substantial portions of the Software.
-//     
-//     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//     FITNESS FOR A PARTICULAR PURPOSE AND NONINFINGEMENT. IN NO EVENT SHALL THE
-//     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-//     SOFTWARE.
-
-using System;
+﻿using System;
 using System.Globalization;
-using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
@@ -123,12 +98,22 @@ namespace DIPOL_UF.Jobs
                     AxisParameter.ReferencingSwitchSpeed,
                     UiSettingsProvider.Settings.Get(@"StepMotorRFSSpeed", 95));
                 var i = 0;
-                for(; i < _referenceSearchMaxAttempts; i++)
+                if (UiSettingsProvider.Settings.Get(@"StepMotorRFSSwitchStatus") is int switchStatus)
                 {
-                    Helper.WriteLog(Serilog.Events.LogEventLevel.Information, @"Reference search, step {i}", i + 1);
-                    await motor.ReferenceReturnToOriginAsync(token);
-                    if (await motor.GetAxisParameter(AxisParameter.ReferenceSwitchStatus) == 1)
-                        break;
+
+                    for (; i < _referenceSearchMaxAttempts; i++)
+                    {
+                        Helper.WriteLog(Serilog.Events.LogEventLevel.Information, @"Reference search, step {i}", i + 1);
+                        await motor.ReferenceReturnToOriginAsync(token);
+                        if (await motor.GetAxisParameter(AxisParameter.ReferenceSwitchStatus) == switchStatus)
+                            break;
+                    }
+                }
+                else
+                {
+                    throw new InvalidOperationException(
+                        "`StepMotorRFSSwitchStatus` should be set in configuration file!"
+                    );
                 }
 
                 Helper.WriteLog(
