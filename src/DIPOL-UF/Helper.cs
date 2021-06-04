feed2dc97b7bf412a0c2cf9c3b11dbdd78a08b5b
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2021-06-04 16:44:12 +0300

    Refactoring

--- src/DIPOL-UF/Helper.cs
+++ src/DIPOL-UF/Helper.cs
@@ -1,28 +1,4 @@
-﻿//    This file is part of Dipol-3 Camera Manager.
-
-//     MIT License
-//     
-//     Copyright(c) 2018-2019 Ilia Kosenkov
-//     
-//     Permission is hereby granted, free of charge, to any person obtaining a copy
-//     of this software and associated documentation files (the "Software"), to deal
-//     in the Software without restriction, including without limitation the rights
-//     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//     copies of the Software, and to permit persons to whom the Software is
-//     furnished to do so, subject to the following conditions:
-//     
-//     The above copyright notice and this permission notice shall be included in all
-//     copies or substantial portions of the Software.
-//     
-//     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//     FITNESS FOR A PARTICULAR PURPOSE AND NONINFINGEMENT. IN NO EVENT SHALL THE
-//     AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//     LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-//     SOFTWARE.
-
-using System;
+﻿using System;
 using System.Buffers;
 using System.Collections;
 using System.ComponentModel;
@@ -44,8 +20,6 @@ using DIPOL_UF.Converters;
 using DynamicData;
 using DynamicData.Binding;
 using ReactiveUI;
-using Serializers;
-using Serilog.Core;
 using Serilog.Events;
 
 namespace DIPOL_UF
@@ -65,13 +39,14 @@ namespace DIPOL_UF
 
             return (bool) (showingAsDialogField?.GetValue(window) ?? false);
         }
-        public static void WriteLog(string entry) => Injector.GetLogger().Write(LogEventLevel.Information, entry);
+        public static void WriteLog(string entry) => Injector.GetLogger()?.Write(LogEventLevel.Information, entry);
 
         public static void WriteLog(ANDOR_CS.Exceptions.AndorSdkException entry) => WriteLog($"{entry.Message} [{entry.ErrorCode}]");
 
         public static void WriteLog(object entry) => WriteLog(entry.ToString());
 
-        public static void WriteLog(LogEventLevel level, string template, params object[] args) => Injector.GetLogger().Write(level, template, args);
+        public static void WriteLog(LogEventLevel level, string template, params object[] args) =>
+            Injector.GetLogger()?.Write(level, template, args);
         public static void WriteLog(LogEventLevel level, Exception exception, string template, params object[] args) => 
             Injector.GetLogger()?.Write(level, exception, template, args);
 
@@ -470,7 +445,7 @@ namespace DIPOL_UF
             [CallerMemberName]
             string name = null)
         {
-            @this.Subscribe(x => LogTask(name));
+            @this.Subscribe(_ => LogTask(name));
             return @this;
         }
 
