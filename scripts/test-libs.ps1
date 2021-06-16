44ec0980f0d4714286a3f0e287239f610c6d30b1
Ilia Kosenkov <ilia.kosenkov@outlook.com>
2021-06-16 18:48:25 +0300

    Pulling test data in script

--- scripts/test-libs.ps1
+++ scripts/test-libs.ps1
@@ -1,2 +1,7 @@
 param([string]$path)
+if ((Get-ChildItem "$path/TestData" -ErrorAction SilentlyContinue | Measure-Object).Count -eq 0) {
+    New-Item -Type Directory -ErrorAction SilentlyContinue "$path/TestData"
+    Invoke-WebRequest https://fits.gsfc.nasa.gov/samples/UITfuv2582gc.fits -OutFile "$path/TestData/UITfuv2582gc.fits"
+    Invoke-WebRequest https://fits.gsfc.nasa.gov/nrao_data/tests/nost_headers/bitpix13.fits -OutFile "$path/TestData/unsupp_bitpix.fits"
+}
 dotnet test $path -c Release 
